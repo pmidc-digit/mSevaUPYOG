fc4cc0258c18aab09501a9cc5e54cc23bb3f0164
Ajay Deepak Morisetty <morisettydeepak@kpmg.com>
2024-12-22 14:51:53 +0530

    sso redirection changes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/NavigationPage/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/employee/NavigationPage/index.js
@@ -1,68 +1,143 @@
-import React, { useEffect } from "react";
-import { useLocation, useHistory, Route, Switch, Redirect } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useHistory } from "react-router-dom";
+import { Loader, Toast } from "@upyog/digit-ui-react-components";
+import { useTranslation } from "react-i18next";
+
+const setEmployeeDetail = (userObject, token) => {
+  let locale = JSON.parse(sessionStorage.getItem("Digit.locale"))?.value || "en_IN";
+  localStorage.setItem("Employee.tenant-id", userObject?.tenantId);
+  localStorage.setItem("tenant-id", userObject?.tenantId);
+  //localStorage.setItem("citizen.userRequestObject", JSON.stringify(userObject));
+  localStorage.setItem("locale", locale);
+  localStorage.setItem("Employee.locale", locale);
+  localStorage.setItem("token", token);
+  localStorage.setItem("Employee.token", token);
+  localStorage.setItem("user-info", JSON.stringify(userObject));
+  localStorage.setItem("Employee.user-info", JSON.stringify(userObject));
+};
 
 const NavigationApp = () => {
+  const { t } = useTranslation();
+  const [isLoading, setIsLoading] = useState(true);
+
   const location = useLocation();
-  const history = useHistory();
+  //const history = useHistory();
+
+  const [userName, setUserName] = useState("");
+  const [tokenName, setTokenName] = useState("");
+  const [serviceName, setServiceName] = useState("");
+  const [navigateToUrl, setNavigateToUrl] = useState("");
+  const [user, setUser] = useState(null);
+  const [showToast, setShowToast] = useState(null);
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
-    const userName = queryParams.get("userName");
-    const tokenName = queryParams.get("token");
-    const serviceName = queryParams.get("serviceName");
-    console.log("In NavigationApp index.js: ", userName, tokenName, serviceName);
+    setUserName(queryParams.get("userName"));
+    setTokenName(queryParams.get("token"));
+    setServiceName(queryParams.get("serviceName"));
+  }, []);
+
+  useEffect(() => {
+    if (userName && tokenName) {
+      callSsoAuthenticateUserApi();
+    }
+    setIsLoading(false);
+  }, [userName, tokenName]);
+
+  const callSsoAuthenticateUserApi = () => {
     const payload = {
       userName: userName,
       tokenName: tokenName,
     };
     Digit.HRMSService.ssoAuthenticateUser(payload)
       .then((result) => {
-        console.log("Result of ssoAuthenticateUser api: ", result);
         if (result.trim() === "Authentication Failed. Employee not found in Eseva") {
-          history.push("/unauthorized");
+          setIsLoading(false);
+          setShowToast("Authentication Failed. Employee not found in Eseva");
+          setTimeout(closeToast, 5000);
           return;
         }
-        const navigateToUrl = result;
-        const servicePath = fetchServicePath(serviceName);
-        if (servicePath) {
-          console.log("Navigate to: ", navigateToUrl, servicePath, navigateToUrl + servicePath);
-          window.location.href = navigateToUrl + servicePath;
-        } else {
-          history.push("/service-not-found");
-          return;
-        }
+        setNavigateToUrl(result);
+        callOauthTokenApi(result);
       })
       .catch((err) => {
-        console.log("Error in HRMSService.ssoAuthenticateUser: " + JSON.stringify(err));
+        setIsLoading(false);
+        console.log("Error in HRMSService.ssoAuthenticateUser: ", err.response);
+        setShowToast("Something went wrong");
+        setTimeout(closeToast, 5000);
       });
-  }, []);
+  };
 
-  const fetchServicePath = (serviceName) => {
-    if (serviceName === "ws-search") {
-      console.log("In ws-search");
-      return "wns/search";
+  const callOauthTokenApi = async (data) => {
+    const payload = {
+      username: "DeepakTest", //userName,
+      password: "eGov@123", // tokenName,
+      tenantId: "pb.testing", //user?.info?.tenantId,
+      userType: "EMPLOYEE", //user?.info?.type
+      //thirdPartyName: "eSewa",
+    };
+
+    try {
+      const { UserRequest: info, ...tokens } = await Digit.UserService.authenticate(payload);
+      Digit.SessionStorage.set("Employee.tenantId", info?.tenantId);
+      setUser({ info, ...tokens });
+    } catch (err) {
+      setIsLoading(false);
+      console.log("Error calling Digit.UserService.authenticate: ", err.response);
+      setShowToast(err?.response?.data?.error_description || "Invalid login credentials");
+      setTimeout(closeToast, 5000);
     }
   };
 
-  console.log("In NavigationApp: ", "History: ", history, "\nLocation: ", location);
+  useEffect(() => {
+    if (user) {
+      //Digit.SessionStorage.set("citizen.userRequestObject", user);
+      const filteredRoles = user?.info?.roles?.filter((role) => role.tenantId === Digit.SessionStorage.get("Employee.tenantId"));
+      if (user?.info?.roles?.length > 0) user.info.roles = filteredRoles;
+      Digit.UserService.setUser(user);
+      setEmployeeDetail(user?.info, user?.access_token);
+      handleServiceRedirection();
+    } else {
+      setIsLoading(false);
+    }
+  }, [user]);
+
+  const closeToast = () => {
+    setShowToast(null);
+  };
+
+  const handleServiceRedirection = () => {
+    const servicePath = fetchServicePath(serviceName);
+    setIsLoading(false);
+    if (servicePath) {
+      window.location.href = navigateToUrl + servicePath;
+    } else {
+      setShowToast("Service not found");
+      setTimeout(closeToast, 5000);
+    }
+  };
+
+  const fetchServicePath = (serviceName) => {
+    switch(serviceName){
+      case "wns-search":
+        return "wns/search";
+      case "pt-search":
+        return "pt-mutation/propertySearch";
+      case "tl-search":
+        return "tradelicence/search";
+      default:
+        return ""; 
+    }
+  };
+
   return (
     <div>
-      {/* <Switch> */}
-      {/* <Route path="/unauthorized" component={UnauthorizedPage} />
-        <Route path="/service-not-found" component={ServiceNotFoundPage} /> */}
-      {/* </Switch> */}
+      {isLoading && <Loader />}
+      {showToast && <Toast error={true} label={t(showToast)} onClose={closeToast} />}
     </div>
   );
 };
 
 export default NavigationApp;
 
-// import React from 'react'
 
-// const index = () => {
-//   return (
-//     <div>navigation app</div>
-//   )
-// }
-
-// export default index

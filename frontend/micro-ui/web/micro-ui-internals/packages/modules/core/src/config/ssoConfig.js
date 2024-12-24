7958bbe815f934a8e20516feafff1033bb32f37e
Deepak <morisettydeepak@kpmg.com>
2024-12-24 16:43:49 +0530

    sso redirection serviceName changes and alert, loader changes.

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/config/ssoConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/config/ssoConfig.js
@@ -1,14 +1,18 @@
 export const serviceMappings = [
   {
-    serviceName: "wns-search",
+    serviceName: "wns",
     path: "wns/search",
   },
   {
-    serviceName: "pt-search",
+    serviceName: "pt-mutation",
     path: "pt-mutation/propertySearch",
   },
   {
-    serviceName: "tl-search",
+    serviceName: "tradelicence",
     path: "tradelicence/search",
+  },
+  {
+    serviceName: "fire-noc",
+    path: "fire-noc/search",
   },
 ];

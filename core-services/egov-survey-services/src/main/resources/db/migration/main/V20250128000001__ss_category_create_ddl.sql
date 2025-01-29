d8e864abc79877a6b0cbaf67f3d59e928cd152ae
suhanikshatriya <suhanikshatriya@kpmg.com>
2025-01-29 19:53:42 +0530

    db file changes

--- core-services/egov-survey-services/src/main/resources/db/migration/main/V20250128000001__ss_category_create_ddl.sql
+++ core-services/egov-survey-services/src/main/resources/db/migration/main/V20250128000001__ss_category_create_ddl.sql
@@ -1,4 +1,4 @@
-CREATE TABLE public.eg_ss_category (
+CREATE TABLE IF NOT EXISTS public.eg_ss_category (
 	id varchar(128) NOT NULL,
 	"label" varchar(128) NOT NULL,
 	tenantid varchar(128) NOT NULL,
@@ -11,3 +11,8 @@ CREATE TABLE public.eg_ss_category (
     CONSTRAINT uk_eg_ss_category_label_tenantid UNIQUE (label, tenantid)
     );
 );
+
+CREATE INDEX IF NOT EXISTS idx_eg_ss_category_id ON eg_ss_category(id);
+CREATE INDEX IF NOT EXISTS idx_eg_ss_category_label ON eg_ss_category("label");
+CREATE INDEX IF NOT EXISTS idx_eg_ss_category_tenantid ON eg_ss_category(tenantid);
+CREATE INDEX IF NOT EXISTS idx_eg_ss_category_isactive ON eg_ss_category(isactive);

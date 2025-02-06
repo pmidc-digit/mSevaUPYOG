af09d8b5f8143a0e9b93841cbcc48282f6812c53
Deepak <morisettydeepak@kpmg.com>
2025-02-06 12:48:52 +0530

    Updated citizen side third party integration code and config

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/NavigationPage/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/core/src/pages/citizen/NavigationPage/index.js
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useHistory } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { Loader, Toast } from "@upyog/digit-ui-react-components";
 import { useTranslation } from "react-i18next";
 import { citizenServiceMappings } from "../../../config/ssoConfig";
 
-const THIRD_PARTY_NAME = "EODB";
 const USER_TYPE = "CITIZEN";
 const TYPE_REGISTER = "register";
 const CITIZEN_ROLE_CODE = "CITIZEN";
+const TOAST_TIMEOUT = 5000;
 //Keep the below values from localisation service (First define these values in localisation service for core module);
-const MISSING_QUERY_PARAMS = "The query parameters provided are not as expected or missing. Please check and try again.";
-const MISSING_QUERY_PARAMS_VALUES = "It looks like some query parameters are missing values. Please provide all required information.";
-const SERVICE_NOT_FOUND = "Service not found";
+const INVALID_QUERY_PARAMS_VALUES = "Some query parameters are missing or invalid. Please provide all required information.";
+const SERVICE_NOT_FOUND = "Requested service not found";
 const REDIRECTING_TO = "Redirecting to";
 const ES_ERROR_USER_NOT_PERMITTED = "ES_ERROR_USER_NOT_PERMITTED";
 const ERROR_MESSAGE = "Something went wrong";
 const ULB_NOT_FOUND = "ULB not found";
+const INVALID_MOBILE_NUMBER = "Mobile number should be 10 digits long";
+const INVALID_USER_NAME = "Invalid user name";
+const ERROR_AUTHENTICATING_USER = "An issue occurred while authenticating. Please try again.";
+const ERROR_CREATING_USER = "An issue occurred while creating the user. Please try again.";
 
 const setCitizenDetail = (userObject, token) => {
   const locale = JSON.parse(sessionStorage.getItem("Digit.initData"))?.value?.selectedLanguage;
@@ -31,8 +34,23 @@ const setCitizenDetail = (userObject, token) => {
   localStorage.setItem("Citizen.user-info", userDetails);
 };
 
+function validateFullName(name) {
+  // Regular expression to check if the name contains only letters and spaces
+  const nameRegex = /^(?=.*[a-zA-Z]{3})[a-zA-Z\s]+$/;
+
+  // Check if the name matches the regular expression
+  return nameRegex.test(name);
+}
+
+function validateMobileNumber(mobileNumber) {
+  // Regular expression to check if the mobile number contains exactly 10 digits
+  const mobileNumberRegex = /^\d{10}$/;
+
+  // Check if the mobile number matches the regular expression
+  return mobileNumberRegex.test(mobileNumber);
+}
+
 const NavigationApp = ({ stateCode }) => {
-  // const history = useHistory();
   const { t } = useTranslation();
   const location = useLocation();
   const [isLoading, setIsLoading] = useState(true);
@@ -41,70 +59,94 @@ const NavigationApp = ({ stateCode }) => {
   const [toastMessage, setToastMessage] = useState(null);
   const [isError, setIsError] = useState(false);
 
-  const { data: cities } = Digit.Hooks.useTenants();
+  const { data: cities, isLoading: isCitiesLoading } = Digit.Hooks.useTenants();
   const getCity = (ULBName) => {
-    const city = cities.find((city) => city.name.toLowerCase() === ULBName.toLowerCase());
+    const city = cities?.find((city) => city.name.toLowerCase() === ULBName.toLowerCase());
     return city;
   };
 
   useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    const expectedParams = ["userName", "token", "serviceName", "data"];
-    const actualParams = Array.from(queryParams.keys());
-    const areParamsCorrect = expectedParams.every((param) => actualParams.includes(param)) && expectedParams.length === actualParams.length;
-
-    if (!areParamsCorrect) {
-      showToast(MISSING_QUERY_PARAMS, true);
+    if (isCitiesLoading) return;
+    if (!cities || cities.length === 0) {
+      showToast(ERROR_MESSAGE, true);
       return;
     }
 
-    const userName = queryParams.get("userName");
-    const token = queryParams.get("token");
-    const serviceName = queryParams.get("serviceName");
+    const queryParams = new URLSearchParams(location.search);
     const dataFromURL = JSON.parse(queryParams.get("data"));
-    const areParamsValid = userName && token && serviceName && dataFromURL?.MobileNo && dataFromURL?.ULBName && dataFromURL?.iPin;
+
+    const isMobileNoValid = validateMobileNumber(dataFromURL?.MobileNo?.trim());
+    const isFullNameValid = validateFullName(dataFromURL?.fullName?.trim());
+    const city = getCity(dataFromURL?.ULBName?.trim());
+    const isCityValid = Object.keys(city).length > 0;
+
+    const areParamsValid =
+      dataFromURL?.thirdPartyName &&
+      dataFromURL?.serviceName &&
+      isMobileNoValid &&
+      dataFromURL?.iPin &&
+      dataFromURL?.ULBName &&
+      isCityValid &&
+      isFullNameValid;
+
     if (areParamsValid) {
       setUserDetails({
-        userName,
-        token,
-        serviceName,
+        thirdPartyCode: dataFromURL.thirdPartyName.trim(),
+        serviceName: dataFromURL.serviceName.trim(),
         mobileNumber: dataFromURL.MobileNo,
-        ULBName: dataFromURL.ULBName.toLowerCase(),
         iPin: dataFromURL.iPin,
+        ULBName: dataFromURL.ULBName.toLowerCase().trim(),
+        userName: dataFromURL.fullName.trim(),
+        returnURL: dataFromURL.returnUrl.trim(),
       });
     } else {
-      showToast(MISSING_QUERY_PARAMS_VALUES, true);
+      if (!isMobileNoValid) {
+        showToast(INVALID_MOBILE_NUMBER, true);
+        return;
+      }
+      if (!isFullNameValid) {
+        showToast(INVALID_USER_NAME, true);
+        return;
+      }
+      if (!isCityValid) {
+        showToast(ULB_NOT_FOUND, true);
+        return;
+      }
+      if (!areParamsValid) {
+        showToast(INVALID_QUERY_PARAMS_VALUES, true);
+      }
     }
-  }, []);
+  }, [isCitiesLoading]);
 
   useEffect(() => {
-    const { userName, token, mobileNumber, ULBName, iPin } = userDetails;
-    if (userName && token && mobileNumber && ULBName && iPin) {
+    if (Object.keys(userDetails).length > 0) {
       userFlowFunc();
     }
   }, [userDetails]);
 
   const userFlowFunc = async () => {
     const [sendApiRes, sendApiErr] = await callSendApi();
-    if (sendApiErr || !sendApiRes?.isSuccessful) {
+    if (sendApiRes?.isSuccessful) {
+      const [createRes, createErr] = await callCreateApi();
+      if (createErr) {
+        handleError(createErr?.response?.data?.error?.message || ERROR_CREATING_USER, "Error in Digit.UserService.registerUser");
+        return;
+      }
+      processUser(createRes);
+    } else if (sendApiErr?.response?.data?.error?.code === 400) {
       const [oauthRes, oauthErr] = await callOauthTokenApi();
       if (oauthErr) {
-        handleError(oauthErr, "Error in Digit.UserService.authenticate");
+        handleError(oauthErr?.response?.data?.error_description || ERROR_AUTHENTICATING_USER, "Error in Digit.UserService.authenticate");
         return;
       }
       processUser(oauthRes);
     } else {
-      const [createRes, createErr] = await callCreateApi();
-      if (createErr) {
-        handleError(createErr, "Error in Digit.UserService.registerUser");
-        return;
-      }
-      processUser(createRes);
+      handleError(ERROR_MESSAGE, "Error in Digit.UserService.sendOtp");
     }
   };
 
   const callSendApi = async () => {
-    const { userName, mobileNumber, ULBName, iPin } = userDetails;
+    const { thirdPartyCode, mobileNumber, iPin, ULBName, userName } = userDetails;
     const payload = {
       otp: {
         name: userName,
@@ -112,7 +154,7 @@ const NavigationApp = ({ stateCode }) => {
         tenantId: stateCode,
         mobileNumber: mobileNumber,
         type: TYPE_REGISTER,
-        thirdPartyName: THIRD_PARTY_NAME,
+        thirdPartyName: thirdPartyCode,
         iPin: iPin,
       },
     };
@@ -126,14 +168,14 @@ const NavigationApp = ({ stateCode }) => {
   };
 
   const callCreateApi = async () => {
-    const { userName, mobileNumber, ULBName, iPin } = userDetails;
+    const { thirdPartyCode, mobileNumber, iPin, ULBName, userName } = userDetails;
     const payload = {
       name: userName,
       username: mobileNumber,
       otpReference: iPin,
       tenantId: `${stateCode}.${ULBName}`,
       permanentCity: `${stateCode}.${ULBName}`,
-      clientId: THIRD_PARTY_NAME,
+      clientId: thirdPartyCode,
     };
 
     try {
@@ -145,13 +187,13 @@ const NavigationApp = ({ stateCode }) => {
   };
 
   const callOauthTokenApi = async () => {
-    const { mobileNumber, iPin } = userDetails;
+    const { thirdPartyCode, mobileNumber, iPin } = userDetails;
     const payload = {
       username: mobileNumber,
       password: iPin,
       tenantId: stateCode,
       userType: USER_TYPE,
-      thirdPartyName: THIRD_PARTY_NAME,
+      thirdPartyName: thirdPartyCode,
     };
 
     try {
@@ -165,7 +207,8 @@ const NavigationApp = ({ stateCode }) => {
   const processUser = (userResponse) => {
     const { UserRequest: info, ...tokens } = userResponse;
     const isAuthorized =
-      info.roles.some((userRole) => userRole.code === CITIZEN_ROLE_CODE) && info.roles.some((userRole) => userRole.code === THIRD_PARTY_NAME);
+      info.roles.some((userRole) => userRole.code === CITIZEN_ROLE_CODE) &&
+      info.roles.some((userRole) => userRole.code === userDetails.thirdPartyCode);
     if (!isAuthorized) {
       showToast(ES_ERROR_USER_NOT_PERMITTED, true);
       return;
@@ -186,7 +229,8 @@ const NavigationApp = ({ stateCode }) => {
       }
 
       const city = getCity(userDetails.ULBName);
-      if (!city) {
+      const isCityValid = Object.keys(city).length > 0;
+      if (!isCityValid) {
         showToast(ULB_NOT_FOUND, true);
         return;
       }
@@ -195,39 +239,37 @@ const NavigationApp = ({ stateCode }) => {
       Digit.SessionStorage.set("citizen.userRequestObject", user);
       Digit.UserService.setUser(user);
       setCitizenDetail(user?.info, user?.access_token);
+      localStorage.setItem("thirdPartyReturnUrl", userDetails?.returnURL);
       handleServiceRedirection(servicePath);
     }
   }, [user]);
 
   const handleServiceRedirection = (servicePath) => {
     //Redirect to the service path:
-    //const newPath = `/citizen/${servicePath}`;
-    //history.replace(newPath);
     const domain = `${window.location.protocol}//${window.location.hostname}:${window.location.port}`;
-    const newURL=`${domain}/citizen/${servicePath}`;
-    window.location.href=newURL;
+    const newURL = `${domain}/citizen/${servicePath}`;
+    window.location.href = newURL;
     showToast(`${REDIRECTING_TO} ${servicePath}`, false);
   };
 
   const fetchServicePath = (serviceName) => {
-    return citizenServiceMappings.find((item) => item.serviceName === serviceName)?.path;
+    return citizenServiceMappings?.find((item) => item.serviceName === serviceName)?.path;
   };
 
   const showToast = (message, isError) => {
     setIsLoading(false);
     setToastMessage(message);
     setIsError(isError);
-    setTimeout(closeToast, 5000);
+    setTimeout(closeToast, TOAST_TIMEOUT);
   };
 
   const closeToast = () => {
     setToastMessage(null);
   };
 
-  const handleError = (err, logMessage) => {
+  const handleError = (errMessage, logMessage) => {
     setIsLoading(false);
-    //console.log(logMessage, err.response);
-    showToast(err?.response?.data?.error?.message || ERROR_MESSAGE, true);
+    showToast(errMessage || ERROR_MESSAGE, true);
   };
 
   return (

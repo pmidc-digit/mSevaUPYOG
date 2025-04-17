db8334d588003c657c21dea993a95c5a5a9c2395
anujkit <chauhan.chauhan5@gmail.com>
2025-04-17 13:00:01 +0530

    firenoc fix

--- municipal-services/firenoc-services/src/api/search.js
+++ municipal-services/firenoc-services/src/api/search.js
@@ -158,7 +158,8 @@ export const searchApiResponse = async (request, next = {}) => {
     //firenocIdQuery = `${firenocIdQuery} )`;
     console.log("Firenoc ID Query -> " + firenocIdQuery);
     const dbResponse = await db.query(firenocIdQuery);
-   // const dbResponse={"command":"SELECT","rowCount":1,"oid":null,"rows":[{"fid":"5ef0340c-38d2-4f7b-be36-0df89c80d98a"}],"fields":[{"name":"fid","tableID":1465020,"columnID":1,"dataTypeID":1043,"dataTypeSize":-1,"dataTypeModifier":68,"format":"text"}],"_parsers":[null],"_types":{"_types":{"arrayParser":{},"builtins":{"BOOL":16,"BYTEA":17,"CHAR":18,"INT8":20,"INT2":21,"INT4":23,"REGPROC":24,"TEXT":25,"OID":26,"TID":27,"XID":28,"CID":29,"JSON":114,"XML":142,"PG_NODE_TREE":194,"SMGR":210,"PATH":602,"POLYGON":604,"CIDR":650,"FLOAT4":700,"FLOAT8":701,"ABSTIME":702,"RELTIME":703,"TINTERVAL":704,"CIRCLE":718,"MACADDR8":774,"MONEY":790,"MACADDR":829,"INET":869,"ACLITEM":1033,"BPCHAR":1042,"VARCHAR":1043,"DATE":1082,"TIME":1083,"TIMESTAMP":1114,"TIMESTAMPTZ":1184,"INTERVAL":1186,"TIMETZ":1266,"BIT":1560,"VARBIT":1562,"NUMERIC":1700,"REFCURSOR":1790,"REGPROCEDURE":2202,"REGOPER":2203,"REGOPERATOR":2204,"REGCLASS":2205,"REGTYPE":2206,"UUID":2950,"TXID_SNAPSHOT":2970,"PG_LSN":3220,"PG_NDISTINCT":3361,"PG_DEPENDENCIES":3402,"TSVECTOR":3614,"TSQUERY":3615,"GTSVECTOR":3642,"REGCONFIG":3734,"REGDICTIONARY":3769,"JSONB":3802,"REGNAMESPACE":4089,"REGROLE":4096}},"text":{},"binary":{}},"RowCtor":null,"rowAsArray":false,"_prebuiltEmptyResultObject":{"fid":null}};
+    
+
 
     let firenocIds = [];
     console.log("dbResponse" + JSON.stringify(dbResponse));
@@ -187,9 +188,9 @@ export const searchApiResponse = async (request, next = {}) => {
         sqlQuery = `${sqlQuery} '${ids[i]}' `;
         if (i != ids.length - 1) sqlQuery = `${sqlQuery} ,`;
       }
-        sqlQuery = `${sqlQuery} ) AND`;
+       // sqlQuery = `${sqlQuery} ) AND`;
     }
-    if (ids.length > 1) sqlQuery = `${sqlQuery} ) AND`;
+    if (ids.length > 0) sqlQuery = `${sqlQuery} ) AND`;
     else 
     {
       return response;

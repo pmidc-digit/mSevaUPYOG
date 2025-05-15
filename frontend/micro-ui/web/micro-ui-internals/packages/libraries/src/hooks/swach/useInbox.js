4afece69628b383a5c5c1896cd3c9db566329770
narinderkpmg <narinderkumar1@kmpg.com>
2025-05-15 11:27:13 +0530

    fix: filters

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/swach/useInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/swach/useInbox.js
@@ -28,7 +28,7 @@ const useSWACHInbox = ({ tenantId, filters = {}, config }) => {
       ...(status?.length > 0 ? { status: status?.map((item) => item?.code) } : {}),
       ...(applicationStatus?.length > 0 ? { status: applicationStatus } : {}),
       // ...(uuid && Object.keys(uuid)?.length > 0 ? { assignee: uuid?.code === "ASSIGNED_TO_ME" ? USER_UUID : "" } : {}),
-      ...(userId ? { assignee: userId ? userId : "" } : {}),
+      // ...(userId ? { assignee: userId ? userId : "" } : {}),
     },
     moduleSearchCriteria: {
       ...(mobileNumber ? { mobileNumber: mobileNumber } : {}),

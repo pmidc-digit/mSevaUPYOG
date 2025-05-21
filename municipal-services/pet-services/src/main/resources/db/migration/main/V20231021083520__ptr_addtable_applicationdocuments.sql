8c73b9059b0f52cdce6a285610fcb6a94ba4c687
Sharan_Gakhar <sgakhar_be21@thapar.edu>
2025-05-21 10:14:38 +0530

    New Service Creation : Pet Services (PNS - 426)
    New Service Creation : Pet Services (PNS - 426)
    Created pet service apis for creating, updating and searching application.

---
+++ municipal-services/pet-services/src/main/resources/db/migration/main/V20231021083520__ptr_addtable_applicationdocuments.sql
@@ -0,0 +1,18 @@
+-- Table: public.eg_ptr_applicationdocuments
+
+-- DROP TABLE IF EXISTS public.eg_ptr_applicationdocuments;
+
+CREATE TABLE IF NOT EXISTS public.eg_ptr_applicationdocuments
+(
+    id character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    tenantid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    documenttype character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    filestoreid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    documentuid character varying(128) COLLATE pg_catalog."default",
+    petapplicationid character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    active boolean,
+    createdby character varying(64) COLLATE pg_catalog."default" NOT NULL,
+    lastmodifiedby character varying(64) COLLATE pg_catalog."default",
+    createdtime bigint,
+    lastmodifiedtime bigint 
+);

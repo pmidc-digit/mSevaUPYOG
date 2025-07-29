24a6c8c9bc9bd91009c8fdc28711e8011b2349e7
narinderkpmg <narinderkumar1@kmpg.com>
2025-07-29 14:28:38 +0530

    fix: chb

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/chb/src/pageComponents/CHBStepper/CHBStepperForm.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/chb/src/pageComponents/CHBStepper/CHBStepperForm.js
@@ -112,7 +112,7 @@ const NewADSStepperForm = () => {
   return (
     <div className="pageCard">
       <CardHeader styles={{ fontSize: "28px", fontWeight: "400", color: "#1C1D1F" }} divider={true}>
-        {t("ADS_REGISTRATION_APPLICATION")}
+        {t("CHB_REGISTRATION_APPLICATION")}
       </CardHeader>
       <Stepper stepsList={updatedCreateEmployeeconfig} onSubmit={handleSubmit} step={step} setStep={setStep} />
       {showToast && (

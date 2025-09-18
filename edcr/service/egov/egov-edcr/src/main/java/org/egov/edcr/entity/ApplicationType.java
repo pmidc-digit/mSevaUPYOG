61e2998ab2cb80a71a3342315a884de538f035b0
Ajay Kumar Bairwa <122883638+ajaybairwa7877@users.noreply.github.com>
2025-09-18 10:44:37 +0530

    Change application type "Building  plan scrutiny" to  "BUILDING_PLAN_SCRUTINY"

--- edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/entity/ApplicationType.java
+++ edcr/service/egov/egov-edcr/src/main/java/org/egov/edcr/entity/ApplicationType.java
@@ -4,7 +4,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum ApplicationType {
 
-    PERMIT("Permit"), OCCUPANCY_CERTIFICATE("Occupancy certificate") , BUILDING_PLAN_SCRUTINY("Building Plan Scrutiny");
+    PERMIT("Permit"), OCCUPANCY_CERTIFICATE("Occupancy certificate") , 
+    BUILDING_PLAN_SCRUTINY("BUILDING_PLAN_SCRUTINY");
 
     @JsonValue
     private final String applicationTypeVal;

057ea68913e87c594dc3bd5dea9c8774548ce985
narinderkpmg <narinderkumar1@kmpg.com>
2025-10-27 11:48:27 +0530

    fix: challan generation

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/challanGeneration/src/pageComponents/OffenderDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/challanGeneration/src/pageComponents/OffenderDetails.js
@@ -36,36 +36,7 @@ const OffenderDetails = ({ t, goNext, currentStepData }) => {
     <React.Fragment>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div style={{ width: "50%" }}>
-          <div style={{ marginBottom: "20px" }}>
-            <CardLabel>
-              {`${t("BPA_BASIC_DETAILS_APPLICATION_NAME_LABEL")}`} <span style={{ color: "red" }}>*</span>
-            </CardLabel>
-            <Controller
-              control={control}
-              name="name"
-              rules={{
-                required: "Name is required",
-                minLength: { value: 2, message: "Name must be at least 2 characters" },
-              }}
-              render={(props) => (
-                <TextInput
-                  style={{ marginBottom: 0 }}
-                  value={props.value}
-                  error={errors?.name?.message}
-                  onChange={(e) => {
-                    props.onChange(e.target.value);
-                  }}
-                  onBlur={(e) => {
-                    props.onBlur(e);
-                  }}
-                  t={t}
-                />
-              )}
-            />
-            {errors?.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
-          </div>
-
-          <div style={{ marginBottom: "20px" }}>
+          {/* <div style={{ marginBottom: "20px" }}>
             <CardLabel>
               {`${t("NOC_APPLICANT_EMAIL_LABEL")}`} <span style={{ color: "red" }}>*</span>
             </CardLabel>
@@ -94,7 +65,7 @@ const OffenderDetails = ({ t, goNext, currentStepData }) => {
               )}
             />
             {errors?.emailId && <p style={{ color: "red" }}>{errors.emailId.message}</p>}
-          </div>
+          </div> */}
 
           <div style={{ marginBottom: "20px" }}>
             <CardLabel>
@@ -121,6 +92,35 @@ const OffenderDetails = ({ t, goNext, currentStepData }) => {
               )}
             />
             {errors?.mobileNumber && <p style={{ color: "red" }}>{errors.mobileNumber.message}</p>}
+          </div>
+
+          <div style={{ marginBottom: "20px" }}>
+            <CardLabel>
+              {`${t("BPA_BASIC_DETAILS_APPLICATION_NAME_LABEL")}`} <span style={{ color: "red" }}>*</span>
+            </CardLabel>
+            <Controller
+              control={control}
+              name="name"
+              rules={{
+                required: "Name is required",
+                minLength: { value: 2, message: "Name must be at least 2 characters" },
+              }}
+              render={(props) => (
+                <TextInput
+                  style={{ marginBottom: 0 }}
+                  value={props.value}
+                  error={errors?.name?.message}
+                  onChange={(e) => {
+                    props.onChange(e.target.value);
+                  }}
+                  onBlur={(e) => {
+                    props.onBlur(e);
+                  }}
+                  t={t}
+                />
+              )}
+            />
+            {errors?.name && <p style={{ color: "red" }}>{errors.name.message}</p>}
           </div>
 
           <div style={{ marginBottom: "20px" }}>

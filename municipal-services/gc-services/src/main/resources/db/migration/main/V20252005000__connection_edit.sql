e3c0fa99de2c26113a995fb489742674fe36d332
vidhi Rajvanshi <101122567+programvidhi89@users.noreply.github.com>
2025-11-28 20:09:08 +0530

    added updated migration script

--- municipal-services/gc-services/src/main/resources/db/migration/main/V20252005000__connection_edit.sql
+++ municipal-services/gc-services/src/main/resources/db/migration/main/V20252005000__connection_edit.sql
@@ -4,7 +4,7 @@ ALTER TABLE eg_gc_connection
     ADD COLUMN plot_size VARCHAR(64),
     ADD COLUMN location VARCHAR(256),
     ADD COLUMN frequency_of_garbage_collection VARCHAR(64),
-    ADD COLUMN type_of_waste VARCHAR(64)
+    ADD COLUMN type_of_waste VARCHAR(64);
 
 
 

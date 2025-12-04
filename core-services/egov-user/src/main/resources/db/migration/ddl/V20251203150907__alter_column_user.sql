dfa464e62345934d6181818d98619f1e12fac333
vidhi Rajvanshi <101122567+programvidhi89@users.noreply.github.com>
2025-12-04 09:54:30 +0530

    modified eg_user table

--- core-services/egov-user/src/main/resources/db/migration/ddl/V20251203150907__alter_column_user.sql
+++ core-services/egov-user/src/main/resources/db/migration/ddl/V20251203150907__alter_column_user.sql
@@ -1,12 +1,3 @@
-
-ALTER TABLE eg_user
-ADD COLUMN permanentDistrict VARCHAR(100),
-ADD COLUMN correspondenceState VARCHAR(100),
-ADD COLUMN permanentState VARCHAR(100),
-ADD COLUMN correspondenceDistrict VARCHAR(100);
-
-
-
 ALTER TABLE eg_user_address
 ADD COLUMN state VARCHAR(100),
 ADD COLUMN district VARCHAR(100);

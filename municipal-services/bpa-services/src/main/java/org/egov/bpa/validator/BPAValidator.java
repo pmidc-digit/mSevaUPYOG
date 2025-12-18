2fac976084c06c43b314f008f5bc48c595e79976
Roshan Chaudhary <roshanchoudhary60@gmail.com>
2025-12-18 12:00:09 +0530

    Remove competency check from BPA service

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/validator/BPAValidator.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/validator/BPAValidator.java
@@ -56,8 +56,8 @@ public class BPAValidator {
 	public void validateCreate(BPARequest bpaRequest, Object mdmsData, Map<String, String> values) {
 		mdmsValidator.validateMdmsData(bpaRequest, mdmsData);
 		validateApplicationDocuments(bpaRequest, mdmsData, null, values);
-		if(!bpaRequest.getBPA().getApplicationType().equalsIgnoreCase("BUILDING_OC_PLAN_SCRUTINY"))
-				validateApplication(bpaRequest);
+//		if(!bpaRequest.getBPA().getApplicationType().equalsIgnoreCase("BUILDING_OC_PLAN_SCRUTINY"))
+//				validateApplication(bpaRequest);
 	}
 
 

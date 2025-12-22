e3862a6ef5646af534c336860d84bd077ed25af8
yawerali <yawerali@kpmg.com>
2025-12-22 10:33:12 +0530

    feat: modal and css changes

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/rentAndLease/src/pageComponents/RALModal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/rentAndLease/src/pageComponents/RALModal.js
@@ -1,10 +1,22 @@
-import { Close, FormComposer, Heading, Loader, Modal, Toast } from "@mseva/digit-ui-react-components";
 import React, { useEffect, useState } from "react";
+import { FormComposer, Modal, Toast } from "@mseva/digit-ui-react-components";
+import { Loader } from "../components/Loader";
 import { ModalConfig } from "../config/ModalConfig";
+
+const Heading = (props) => {
+  return <h1 className={`heading-m ${props.className}`}>{props.label}</h1>;
+};
+
+const Close = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#FFFFFF">
+    <path d="M0 0h24v24H0V0z" fill="none" />
+    <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z" />
+  </svg>
+);
 
 const CloseBtn = (props) => {
   return (
-    <div className="icon-bg-secondary" onClick={props.onClick}>
+    <div className={`icon-bg-secondary ${props.className}`} onClick={props.onClick}>
       <Close />
     </div>
   );

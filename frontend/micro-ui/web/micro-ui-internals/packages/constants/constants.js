0acae7b21e71ece32d71c820d2ffb9e9e1aa3351
NayanPMIDC <mujibalam2000@gmail.com>
2026-01-05 23:12:01 +0530

    fix: BPA Regualr Workflow

--- frontend/micro-ui/web/micro-ui-internals/packages/constants/constants.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/constants/constants.js
@@ -3,4 +3,4 @@ export const TENANT_IDS = {
   PATIALA: "pb.patiala"
 };
 
-export const OBPS_BPA_BUSINESS_SERVICES = ["BPA_LOW", "BPA"];
+export const OBPS_BPA_BUSINESS_SERVICES = ["BPA_LOW", "BPA", "BPA_OC", "BPA_MC", "BPA_NP", "BPA_MC_HIGH", "BPA_NP_HIGH", "BPA_NP_OTH", "BPA_MC_OTH"];

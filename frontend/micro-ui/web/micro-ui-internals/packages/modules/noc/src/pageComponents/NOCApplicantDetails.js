30601c4afadc9b62bc66640ec278c97ff99e38ec
saurabh-singh-kpmg <saurabhsingh20@kpmg.com>
2026-01-18 21:19:51 +0530

    css fix and breadcrumb integrated

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pageComponents/NOCApplicantDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pageComponents/NOCApplicantDetails.js
@@ -344,7 +344,10 @@ const NOCApplicantDetails = (_props) => {
                       />
                     )}
                   />
-                  <CardLabelError style={{ fontSize: "12px", marginTop: "4px" }}>{errors?.owners?.[index]?.ownerType?.message || ""}</CardLabelError>
+                
+                 {errors?.owners?.[index]?.ownerType  && (
+                  <p style={{ color: "red", marginBottom: "0" }}>{errors?.owners?.[index]?.ownerType?.message }</p>
+                )}
                 </div>
               </LabelFieldPair>
             )}
--- frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/citizen/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/citizen/index.js
@@ -1,10 +1,42 @@
-import { AppContainer, BackButton, PrivateRoute } from "@mseva/digit-ui-react-components";
+import { AppContainer, BackButton, BreadCrumb, PrivateRoute } from "@mseva/digit-ui-react-components";
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
 import { shouldHideBackButton } from "../../utils";
 import { useTranslation } from "react-i18next";
 
 const hideBackButtonConfig = [];
+
+const NOCBreadCrumbs = ({ location }) => {
+  const { t } = useTranslation();
+  const crumbs = [
+    {
+      path: "/digit-ui/citizen",
+      content: t("ES_COMMON_HOME"),
+      show: true,
+    },
+    {
+      path: "/digit-ui/citizen/noc-home",
+      content: `NOC Home`,
+      show: location.pathname.includes("/noc/new-application") ? true : false,
+    },
+    {
+      path: "/digit-ui/citizen/noc-home",
+      content: `NOC Home`,
+      show: location.pathname.includes("noc/my-application") ? true : false,
+    },
+    {
+      path: "/digit-ui/citizen/noc-home",
+      content: `NOC Home`,
+      show: location.pathname.includes("noc/search/application-overview/") ? true : false,
+    },
+    {
+      path: "/digit-ui/citizen/noc-home",
+      content: `NOC Home`,
+      show: location.pathname.includes("noc/search-application") ? true : false,
+    },
+  ];
+  return <BreadCrumb crumbs={crumbs} />;
+};
 
 const App = () => {
   const { path, url, ...match } = useRouteMatch();
@@ -15,12 +47,19 @@ const App = () => {
   const NOCCitizenApplicationOverview = Digit?.ComponentRegistryService?.getComponent("NOCCitizenApplicationOverview");
   const NewNOCEditApplication = Digit?.ComponentRegistryService?.getComponent("NewNOCEditApplication");
   const NOCCitizenSearchApplication = Digit?.ComponentRegistryService?.getComponent("NOCCitizenSearchApplication");
+  const isResponse = window.location.href.includes("/response");
+  const isMobile = window.Digit.Utils.browser.isMobile();
 
   return (
     <span className={"pgr-citizen-wrapper"} style={{ width: "100%" }}>
       <Switch>
         <AppContainer>
-          {!shouldHideBackButton(hideBackButtonConfig) ? <BackButton>Back</BackButton> : ""}
+           {!isResponse ? (
+            <div style={window.location.href.includes("application-overview") || isMobile ? { marginLeft: "10px" } : {}}>
+              <NOCBreadCrumbs location={location} />
+            </div>
+          ) : null}
+          {/* {!shouldHideBackButton(hideBackButtonConfig) ? <BackButton>Back</BackButton> : ""} */}
           <PrivateRoute path={`${path}/new-application`} component={NewNOCApplication} />
           <PrivateRoute path={`${path}/response/:id`} component={NOCResponseCitizen} />
           <PrivateRoute path={`${path}/my-application`} component={NOCCitizenMyApplications} />

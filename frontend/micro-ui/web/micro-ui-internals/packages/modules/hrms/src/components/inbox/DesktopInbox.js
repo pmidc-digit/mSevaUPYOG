200a01d7dded573be0cdf0935eccf159e435973a
narinderkpmg <narinderkumar1@kmpg.com>
2026-01-27 19:03:28 +0530

    removed links

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/components/inbox/DesktopInbox.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/components/inbox/DesktopInbox.js
@@ -11,7 +11,11 @@ const DesktopInbox = ({ tableConfig, filterComponent, ...props }) => {
   const tenantIds = Digit.SessionStorage.get("HRMS_TENANTS");
   const GetCell = (value) => <span className="cell-text">{t(value)}</span>;
   const GetSlaCell = (value) => {
-    return value == "INACTIVE" ? <span className="sla-cell-error">{ t(value )|| ""}</span> : <span className="sla-cell-success">{ t(value) || ""}</span>;
+    return value == "INACTIVE" ? (
+      <span className="sla-cell-error">{t(value) || ""}</span>
+    ) : (
+      <span className="sla-cell-success">{t(value) || ""}</span>
+    );
   };
   const data = props?.data?.Employees;
 
@@ -44,7 +48,7 @@ const DesktopInbox = ({ tableConfig, filterComponent, ...props }) => {
             <div className="tooltip">
               {" "}
               {GetCell(`${row.original?.user?.roles.length}`)}
-              <span className="tooltiptext" style={{whiteSpace: "nowrap"}}>
+              <span className="tooltiptext" style={{ whiteSpace: "nowrap" }}>
                 {row.original?.user?.roles.map((ele, index) => (
                   <span>
                     {`${index + 1}. ` + t(`ACCESSCONTROL_ROLES_ROLES_${ele.code}`)} <br />{" "}
@@ -142,25 +146,7 @@ const DesktopInbox = ({ tableConfig, filterComponent, ...props }) => {
     <div className="inbox-container">
       {!props.isSearch && (
         <div className="filters-container">
-          <InboxLinks
-            parentRoute={props.parentRoute}
-            allLinks={[
-              {
-                text: "HR_COMMON_CREATE_EMPLOYEE_HEADER",
-                link: "/digit-ui/employee/hrms/create",
-                businessService: "hrms",
-                roles: ["HRMS_ADMIN"],
-              },
-              {
-                text: "HR_COMMON_OBPAS_EMP_MAPING",
-                link: "/digit-ui/employee/hrms/empMaping",
-                businessService: "hrms",
-                roles: ["HRMS_ADMIN"],
-              },
-            ]}
-            headerText={"HRMS"}
-            businessService={props.businessService}
-          />
+          <InboxLinks parentRoute={props.parentRoute} allLinks={[]} headerText={"HRMS"} businessService={props.businessService} />
           <div>
             {
               <FilterComponent

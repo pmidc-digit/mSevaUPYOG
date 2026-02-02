9a144e50c821728bf24d2063f224213e6a63fd12
roshan2611 <56333820+roshan2611@users.noreply.github.com>
2026-02-02 15:44:23 +0530

    Update App.js

--- frontend/workbench-ui/web/src/App.js
+++ frontend/workbench-ui/web/src/App.js
@@ -60,9 +60,10 @@ initLibraries().then(() => {
 
 function App() {
   window.contextPath = window?.globalConfigs?.getConfig("CONTEXT_PATH");
-  const stateCode =
-    window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") ||
-    process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
+  //const stateCode =
+    //window.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") ||
+    //process.env.REACT_APP_STATE_LEVEL_TENANT_ID;
+  const stateCode = "pb";
   if (!stateCode) {
     return <h1>stateCode is not defined</h1>;
   }

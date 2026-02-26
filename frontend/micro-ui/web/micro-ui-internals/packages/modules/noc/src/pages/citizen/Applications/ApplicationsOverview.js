7789b7a3dbe82c2852e842d0c7a2c827d7ff3f59
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-02-26 14:24:40 +0530

    noc dev observations

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/citizen/Applications/ApplicationsOverview.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/citizen/Applications/ApplicationsOverview.js
@@ -109,8 +109,7 @@ const CitizenApplicationOverview = () => {
        documents: applicationDetails?.Noc?.[0]?.nocDetails?.additionalDetails?.siteImages
    } : {})
   
-  // console.log('applicationD', applicationDetails)
- const [approverComment , setApproverComment] = useState(null);
+  console.log('applicationD', applicationDetails)
   // const latestCalc = applicationDetails?.Noc?.[0]?.nocDetails?.additionalDetails?.calculations?.find(c => c.isLatest);
 
   const { data: storeData } = Digit.Hooks.useStore.getInitData();

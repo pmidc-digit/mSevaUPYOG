b1979d88e296330325035beeeef4ffeaa4ba474a
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-03-05 19:08:15 +0530

    OB-1070 - show other owner names with firmname in declaration statement

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/employee/ApplicationOverview/ApplicationOverview.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/employee/ApplicationOverview/ApplicationOverview.js
@@ -1129,7 +1129,9 @@ const validateSiteImages = (action) => {
 
   const ownersList = applicationDetails?.Noc?.[0]?.nocDetails.additionalDetails?.applicationDetails?.owners?.map((item) => item.ownerOrFirmName);
   const firmName = applicationDetails?.Noc?.[0]?.nocDetails.additionalDetails?.applicationDetails?.owners?.[0]?.firmName
-  const combinedOwnersName = firmName?.trim() || ownersList?.join(", ");
+  const combinedOwnersName = [...(firmName?.trim() ? [firmName.trim()] : []), ...(ownersList || [])]
+    .filter((v, i, arr) => v && arr.indexOf(v) === i)
+    .join(", ");
   const primaryOwner = displayData?.applicantDetails?.[0]?.owners?.[0];
   const propertyId = displayData?.applicantDetails?.[0]?.owners?.[0]?.propertyId;
 

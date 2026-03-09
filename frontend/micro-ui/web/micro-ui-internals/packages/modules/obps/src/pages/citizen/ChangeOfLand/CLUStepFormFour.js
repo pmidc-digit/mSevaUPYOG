60dc0fab83cb9cb5d0b686d5f6cd01a37efc1744
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-03-09 20:17:18 +0530

    clu summary page declaration fix as per feedback
    fixed to obtain lates data from redux and not stale data from api

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ChangeOfLand/CLUStepFormFour.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ChangeOfLand/CLUStepFormFour.js
@@ -76,7 +76,6 @@ const CLUStepFormFour = ({ config, onGoNext, onBackClick, t }) => {
 
     try {
       const finalPayload = mapToCLUPayload(data, selectedAction);
-      console.log("finalPayload here==>", finalPayload);
 
       const response = await Digit.OBPSService.CLUUpdate({ tenantId, details: finalPayload });
 
@@ -280,7 +279,7 @@ const CLUStepFormFour = ({ config, onGoNext, onBackClick, t }) => {
     //console.log("selectedAction here", action);
   }
 
-  const ownersList = currentStepData?.apiData?.Clu?.[0]?.cluDetails?.additionalDetails?.applicationDetails?.owners?.map(
+  const ownersList = currentStepData?.applicationDetails?.owners?.map(
     (item) => item.ownerOrFirmName
   );
   console.log("ownersList===>", ownersList);

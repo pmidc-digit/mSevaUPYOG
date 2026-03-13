09f29273d56c468f2f2c48b47b346d1961e8444b
Sharan_Gakhar <sgakhar_be21@thapar.edu>
2026-03-13 14:54:25 +0530

    LTS changes for egov searcher
    LTS changes for egov searcher

--- core-services/egov-searcher/src/main/java/org/egov/search/controller/CxfConfig.java
+++ core-services/egov-searcher/src/main/java/org/egov/search/controller/CxfConfig.java
@@ -1,38 +1,27 @@
-/**
- * 
- */
 package org.egov.search.controller;
 
 import jakarta.xml.ws.Endpoint;
-import org.apache.cxf.metrics.MetricsFeature;
-import org.apache.cxf.metrics.MetricsProvider;
 import org.apache.cxf.Bus;
 import org.apache.cxf.jaxws.EndpointImpl;
 import org.egov.search.service.SearchService;
 import org.egov.search.webservice.SearchSoapServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**	
- * 
- */
 @Configuration
 public class CxfConfig {
 
-	@Autowired
-    private Bus bus;
+    private final Bus bus;
+    private final SearchService searchService;
 
-    @Autowired
-    private MetricsProvider metricsProvider;
-    @Autowired
-	private SearchService searchService;
+    public CxfConfig(Bus bus, SearchService searchService) {
+        this.bus = bus;
+        this.searchService = searchService;
+    }
 
     @Bean
     public Endpoint endpoint() {
-        EndpointImpl endpoint = new EndpointImpl(bus, new SearchSoapServiceImpl(searchService), null, null, new MetricsFeature[]{
-            new MetricsFeature(metricsProvider)
-        });
+        EndpointImpl endpoint = new EndpointImpl(bus, new SearchSoapServiceImpl(searchService));
         endpoint.publish("/SearchPMIDC");
         return endpoint;
     }

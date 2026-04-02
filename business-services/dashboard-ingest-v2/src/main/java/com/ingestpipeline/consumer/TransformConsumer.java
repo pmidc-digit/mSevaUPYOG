4d01cafb3d977b89e226eca312f8eb40dd14d23f
jashan7167 <jashansingh9890@gmail.com>
2026-04-02 14:30:20 +0530

    testing the dashboard-ingest-v2

--- business-services/dashboard-ingest-v2/src/main/java/com/ingestpipeline/consumer/TransformConsumer.java
+++ business-services/dashboard-ingest-v2/src/main/java/com/ingestpipeline/consumer/TransformConsumer.java
@@ -37,7 +37,7 @@ public class TransformConsumer implements KafkaConsumer {
 	private ApplicationProperties applicationProperties; 
 
 	@Override
-	@KafkaListener(id = INTENT, groupId = INTENT, topics = { Constants.KafkaTopics.VALID_DATA }, containerFactory = Constants.BeanContainerFactory.INCOMING_KAFKA_LISTENER)
+	@KafkaListener(id = INTENT, groupId = "${kafka.consumer.group.transform:transform-v2}", topics = { Constants.KafkaTopics.VALID_DATA }, containerFactory = Constants.BeanContainerFactory.INCOMING_KAFKA_LISTENER)
 	public void processMessage(Map incomingData,
 							   @Header(KafkaHeaders.RECEIVED_TOPIC) final String topic) {
 		LOGGER.info("##KafkaMessageAlert## : key:" + topic + ":" + "value:" + incomingData.size());

5c919dfa69b8c8ce256551802cea5ff6b7e9f3a5
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-04-14 10:41:33 +0530

    bpa pay 2 fee receipt mismatch fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/response/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/common/src/payments/citizen/response/index.js
@@ -353,6 +353,7 @@ const WrapPaymentComponent = (props) => {
           } else {
             updatedpayments = {
               ...paymentData,
+               BPA: [bpaData?.[0]],
               additionalDetails: {
                 ...paymentData.additionalDetails,
                 designation: designation,

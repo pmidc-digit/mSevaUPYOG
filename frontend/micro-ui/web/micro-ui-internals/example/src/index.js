37d7cfc3960b0bffc30586ebc066abe538ddadd2
anujkit <chauhan.chauhan5@gmail.com>
2026-04-29 12:48:20 +0530

    disabled module

--- frontend/micro-ui/web/micro-ui-internals/example/src/index.js
+++ frontend/micro-ui/web/micro-ui-internals/example/src/index.js
@@ -2,50 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { initLibraries } from "@mseva/digit-ui-libraries";
-import { PGRReducers } from "@mseva/digit-ui-module-pgr";
-import { SWACHReducers } from "@mseva/digit-ui-module-swach";
-import { HRMSReducers } from "@mseva/digit-ui-module-hrms";
-import { HRMSModule, initHRMSComponents } from "@mseva/digit-ui-module-hrms";
+//import { PGRReducers } from "@mseva/digit-ui-module-pgr";
+//import { SWACHReducers } from "@mseva/digit-ui-module-swach";
+//import { HRMSReducers } from "@mseva/digit-ui-module-hrms";
+//import { HRMSModule, initHRMSComponents } from "@mseva/digit-ui-module-hrms";
 
 import { PTModule, PTLinks, PTComponents, PTReducers } from "@mseva/digit-ui-module-pt";
-import { MCollectModule, MCollectLinks } from "@mseva/digit-ui-module-mcollect";
-import {
-  ChallanGenerationModule,
-  ChallanGenerationLinks,
-  initChallanGenerationComponents,
-  ChallanReducers,
-} from "@mseva/digit-ui-module-challangeneration";
-import {
-  GarbageCollectionModule,
-  GarbageCollectionLinks,
-  initGarbageCollectionComponents,
-  GarbageReducers,
-} from "@mseva/digit-ui-module-garbagecollection";
-import { RentAndLeaseModule, RentAndLeaseLinks,RentAndLeaseReducers } from "@mseva/digit-ui-module-rentandlease";
-import { TLModule, TLLinks, initTLComponents, TLReducers } from "@mseva/digit-ui-module-tl";
-import { initFSMComponents } from "@mseva/digit-ui-module-fsm";
-import { initPGRComponents } from "@mseva/digit-ui-module-pgr";
-import { initSWACHComponents } from "@mseva/digit-ui-module-swach";
-import { initDSSComponents } from "@mseva/digit-ui-module-dss";
-import { initReceiptsComponents, ReceiptsModule } from "@mseva/digit-ui-module-receipts";
+//import { MCollectModule, MCollectLinks } from "@mseva/digit-ui-module-mcollect";
+//import {
+//  ChallanGenerationModule,
+//  ChallanGenerationLinks,
+ // initChallanGenerationComponents,
+ // ChallanReducers,
+//} from "@mseva/digit-ui-module-challangeneration";
+//import {
+ // GarbageCollectionModule,
+ // GarbageCollectionLinks,
+ // initGarbageCollectionComponents,
+ // GarbageReducers,
+//} from "@mseva/digit-ui-module-garbagecollection";
+//import { RentAndLeaseModule, RentAndLeaseLinks,RentAndLeaseReducers } from "@mseva/digit-ui-module-rentandlease";
+//import { TLModule, TLLinks, initTLComponents, TLReducers } from "@mseva/digit-ui-module-tl";
+//import { initFSMComponents } from "@mseva/digit-ui-module-fsm";
+//import { initPGRComponents } from "@mseva/digit-ui-module-pgr";
+//import { initSWACHComponents } from "@mseva/digit-ui-module-swach";
+//import { initDSSComponents } from "@mseva/digit-ui-module-dss";
+//import { initReceiptsComponents, ReceiptsModule } from "@mseva/digit-ui-module-receipts";
 // import { initReportsComponents } from "@mseva/digit-ui-module-reports";
-import { initMCollectComponents } from "@mseva/digit-ui-module-mcollect";
-import { initRentAndLeaseComponents } from "@mseva/digit-ui-module-rentandlease";
+//import { initMCollectComponents } from "@mseva/digit-ui-module-mcollect";
+//import { initRentAndLeaseComponents } from "@mseva/digit-ui-module-rentandlease";
 import { PaymentModule, PaymentLinks, paymentConfigs } from "@mseva/digit-ui-module-common";
-import { initOBPSComponents, OBPSReducers } from "@mseva/digit-ui-module-obps";
+//import { initOBPSComponents, OBPSReducers } from "@mseva/digit-ui-module-obps";
 import { initEngagementComponents, SurveyReducers } from "@mseva/digit-ui-module-engagement";
-import { initNOCComponents, NOCReducers, NOCLinks, NOCModule } from "@mseva/digit-ui-module-noc";
+//import { initNOCComponents, NOCReducers, NOCLinks, NOCModule } from "@mseva/digit-ui-module-noc";
 import { initWSComponents, WSReducers } from "@mseva/digit-ui-module-ws";
 import { DigitUI } from "@mseva/digit-ui-module-core";
 import { initCommonPTComponents, CommonPTModule } from "@mseva/digit-ui-module-commonpt";
-import { initBillsComponents, BillsModule } from "@mseva/digit-ui-module-bills";
-import { PTRModule, PTRLinks, PTRComponents, PTRReducers } from "@mseva/digit-ui-module-ptr";
-import { SVComponents, SVLinks, SVModule } from "@mseva/digit-ui-module-sv";
-import { initNDCComponents, NDCReducers } from "@mseva/digit-ui-module-ndc";
-import { ADSModule, ADSLinks, ADSComponents, ADSReducers } from "@mseva/upyog-ui-module-ads";
-import { CHBModule, CHBLinks, CHBComponents, CHBReducers } from "@mseva/upyog-ui-module-chb";
-import { ASSETComponents, ASSETLinks, ASSETModule } from "@mseva/upyog-ui-module-asset";
-import { PGRAIComponents, PGRAILinks, PGRAIModule } from "@mseva/upyog-ui-module-pgrai";
+//import { initBillsComponents, BillsModule } from "@mseva/digit-ui-module-bills";
+//import { PTRModule, PTRLinks, PTRComponents, PTRReducers } from "@mseva/digit-ui-module-ptr";
+//import { SVComponents, SVLinks, SVModule } from "@mseva/digit-ui-module-sv";
+//import { initNDCComponents, NDCReducers } from "@mseva/digit-ui-module-ndc";
+//import { ADSModule, ADSLinks, ADSComponents, ADSReducers } from "@mseva/upyog-ui-module-ads";
+//import { CHBModule, CHBLinks, CHBComponents, CHBReducers } from "@mseva/upyog-ui-module-chb";
+//import { ASSETComponents, ASSETLinks, ASSETModule } from "@mseva/upyog-ui-module-asset";
+//import { PGRAIComponents, PGRAILinks, PGRAIModule } from "@mseva/upyog-ui-module-pgrai";
 import { initFIRENOCComponents, FIRENOCReducers, FireNOCLinks, FireNOCModule } from "@mseva/digit-ui-module-firenoc";
 
 // import {initCustomisationComponents} from "./customisations";
@@ -55,47 +55,47 @@ import { initFIRENOCComponents, FIRENOCReducers, FireNOCLinks, FireNOCModule } f
 import "@pmidc/upyog-css";
 // import * as comps from "@mseva/digit-ui-react-components";
 
-import { pgrCustomizations, pgrComponents } from "./pgr";
+//import { pgrCustomizations, pgrComponents } from "./pgr";
 var Digit = window.Digit || {};
 
 const enabledModules = [
-  "PGR",
-  "FSM",
+ // "PGR",
+ // "FSM",
   "Payment",
   "PT",
   "QuickPayLinks",
-  "DSS",
-  "MCollect",
-  "HRMS",
-  "TL",
-  "Receipts",
-  "Reports",
-  "OBPS",
-  "Engagement",
-  "NOC",
+ // "DSS",
+ // "MCollect",
+ // "HRMS",
+ // "TL",
+ // "Receipts",
+ // "Reports",
+ // "OBPS",
+ // "Engagement",
+ // "NOC",
   "WS",
   "CommonPT",
-  "NDSS",
-  "Bills",
+  //"NDSS",
+  //"Bills",
   "SW",
-  "BillAmendment",
+  //"BillAmendment",
   "FireNoc",
-  "Birth",
-  "Death",
-  "PTR",
-  "ADS",
-  "Swach",
-  "SV",
-  "NDC",
-  "CHB",
-  "ASSET",
-  "PGRAI",
-  "ChallanGeneration",
-  "RentAndLease",
-  "BPAStakeholder",
+  //"Birth",
+  //"Death",
+  //"PTR",
+  //"ADS",
+  //"Swach",
+  //"SV",
+  //"NDC",
+  //"CHB",
+  //"ASSET",
+  //"PGRAI",
+  //"ChallanGeneration",
+  //"RentAndLease",
+  //"BPAStakeholder",
   "Layout",
-  "GarbageCollection",
-  "CLU",
+  //"GarbageCollection",
+  //"CLU",
   "FIRENOC"
 ];
 
@@ -128,99 +128,99 @@ const initTokens = (stateCode) => {
 
 const initDigitUI = () => {
   window?.Digit.ComponentRegistryService.setupRegistry({
-    ...pgrComponents,
+ //   ...pgrComponents,
     PaymentModule,
     ...paymentConfigs,
     PaymentLinks,
     PTModule,
     PTLinks,
     ...PTComponents,
-    MCollectLinks,
-    MCollectModule,
-    ChallanGenerationModule,
-    ChallanGenerationLinks,
-    GarbageCollectionModule,
-    GarbageCollectionLinks,
-    RentAndLeaseModule,
-    RentAndLeaseLinks,
-    HRMSModule,
-    ReceiptsModule,
-    BillsModule,
-    PTRModule,
-    PTRLinks,
+   // MCollectLinks,
+   // MCollectModule,
+    //ChallanGenerationModule,
+    //ChallanGenerationLinks,
+    //GarbageCollectionModule,
+    //GarbageCollectionLinks,
+    //RentAndLeaseModule,
+    //RentAndLeaseLinks,
+    //HRMSModule,
+    //ReceiptsModule,
+    //BillsModule,
+    //PTRModule,
+    //PTRLinks,
     CommonPTModule,
-    ...PTRComponents,
-    TLModule,
-    TLLinks,
-    SVModule,
-    SVLinks,
-    ...SVComponents,
-    ADSLinks,
-    ADSModule,
-    ...ADSComponents,
-    CHBModule,
-    CHBLinks,
-    ...CHBComponents,
-    ASSETModule,
-    ASSETLinks,
-    ...ASSETComponents,
-    PGRAIModule,
-    PGRAILinks,
-    ...PGRAIComponents,
-    NOCModule,
-    NOCLinks,
+   // ...PTRComponents,
+   // TLModule,
+   // TLLinks,
+    //SVModule,
+    //SVLinks,
+    //...SVComponents,
+    //ADSLinks,
+    //ADSModule,
+    //...ADSComponents,
+    //CHBModule,
+    //CHBLinks,
+    //...CHBComponents,
+    //ASSETModule,
+    //ASSETLinks,
+    //...ASSETComponents,
+    //PGRAIModule,
+    //PGRAILinks,
+    //...PGRAIComponents,
+    //NOCModule,
+    //NOCLinks,
     FireNOCModule,
     FireNOCLinks,
   });
-  initFSMComponents();
-  initPGRComponents();
-  initSWACHComponents();
-  initDSSComponents();
-  initMCollectComponents();
-  initChallanGenerationComponents();
-  initGarbageCollectionComponents();
-  initRentAndLeaseComponents();
-  initHRMSComponents();
-  initTLComponents();
-  initReceiptsComponents();
+ // initFSMComponents();
+ // initPGRComponents();
+ // initSWACHComponents();
+ // initDSSComponents();
+ // initMCollectComponents();
+ // initChallanGenerationComponents();
+ // initGarbageCollectionComponents();
+  //initRentAndLeaseComponents();
+  //initHRMSComponents();
+  //initTLComponents();
+  //initReceiptsComponents();
   // initReportsComponents();
-  initOBPSComponents();
+  //initOBPSComponents();
   initEngagementComponents();
-  initNOCComponents();
+  //initNOCComponents();
   initWSComponents();
   initCommonPTComponents();
-  initBillsComponents();
-  initNDCComponents();
+  //initBillsComponents();
+  //initNDCComponents();
   initFIRENOCComponents();
   // initCustomisationComponents();
 
   const moduleReducers = (initData) => ({
-    pgr: PGRReducers(initData),
-    swach: SWACHReducers(initData),
-    hrms: HRMSReducers(initData),
+    //pgr: PGRReducers(initData),
+    //swach: SWACHReducers(initData),
+    //hrms: HRMSReducers(initData),
     pt: PTReducers(initData),
     ws: WSReducers(initData),
     engagement: SurveyReducers(initData),
-    tl: TLReducers(initData),
-    ndc: NDCReducers(initData),
-    ptr: PTRReducers(initData),
-    ads: ADSReducers(initData),
-    chb: CHBReducers(initData),
-    noc: NOCReducers(initData),
-    obps: OBPSReducers(initData),
-    challan: ChallanReducers(initData),
-    rentAndLease: RentAndLeaseReducers(initData),
-    gc: GarbageReducers(initData),
+    //tl: TLReducers(initData),
+    //ndc: NDCReducers(initData),
+    //ptr: PTRReducers(initData),
+    //ads: ADSReducers(initData),
+    //chb: CHBReducers(initData),
+    //noc: NOCReducers(initData),
+    //obps: OBPSReducers(initData),
+    //challan: ChallanReducers(initData),
+    //rentAndLease: RentAndLeaseReducers(initData),
+    //gc: GarbageReducers(initData),
     firenoc: FIRENOCReducers(initData),
   });
 
   window.Digit.Customizations = {
-    PGR: pgrCustomizations,
-    TL: {
-      customiseCreateFormData: (formData, licenceObject) => licenceObject,
-      customiseRenewalCreateFormData: (formData, licenceObject) => licenceObject,
-      customiseSendbackFormData: (formData, licenceObject) => licenceObject,
-    },
+   // PGR: pgrCustomizations,
+    // TL: {
+    //   customiseCreateFormData: (formData, licenceObject) => licenceObject,
+    //   customiseRenewalCreateFormData: (formData, licenceObject) => licenceObject,
+    //   customiseSendbackFormData: (formData, licenceObject) => licenceObject,
+    // },
   };
 
   const stateCode = window?.globalConfigs?.getConfig("STATE_LEVEL_TENANT_ID") || "pb";

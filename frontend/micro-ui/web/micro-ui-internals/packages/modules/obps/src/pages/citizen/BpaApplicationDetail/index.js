7782ee49c5a31e83dbd6d9a245703340bb81ce55
NayanPMIDC <nayandhawan@kpmg.com>
2026-05-25 13:24:33 +0530

    feat: Employee Side Fee and Sanction Documents

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
@@ -1001,12 +1001,11 @@ useEffect(() => {
        }
        const fileStore = await Digit.PaymentService.printReciept(tenantId, { fileStoreIds: fileStoreId });
        window.open(fileStore[fileStoreId], "_blank");
-       requestData["applicationType"] = data?.applicationData?.additionalDetails?.applicationType;
+      //  requestData["applicationType"] = data?.applicationData?.additionalDetails?.applicationType;
      } catch (error) {
        console.log("error", error);
      } finally {
-       Digit.StoreData.getCurrentLanguage = prevGetLang;
-       setIsEnableLoader(true);
+       setIsEnableLoader(false);
      }
    }
 
@@ -1675,6 +1674,8 @@ useEffect(() => {
       }
     }
   }
+
+  console.log("loader Data", "isLoading:", isLoading, "isEnableLoader:", isEnableLoader, "LicenseDataLoading:", LicenseDataLoading, "isMdmsLoading:", isMdmsLoading, "isLoadingScrutiny:", isLoadingScrutiny, "isMdmsLoadingFees:", isMdmsLoadingFees, "apiLoading:", apiLoading)
 
   if (isLoading || isEnableLoader ||LicenseDataLoading || isMdmsLoading || isLoadingScrutiny || isMdmsLoadingFees || apiLoading) {
     return <Loader />

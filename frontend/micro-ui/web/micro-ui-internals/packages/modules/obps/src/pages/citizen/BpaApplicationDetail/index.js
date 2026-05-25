e95f137ddc388e584f67c7df6e53f045bf6820cf
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-05-25 10:36:02 +0530

    punjabi font not showing in pdfs fixed

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
@@ -777,31 +777,78 @@ useEffect(() => {
   async function getPermitOccupancyOrderSearch({ tenantId }, order, mode = "download") {
     let fileStoreId = data?.applicationData?.additionalDetails?.sanctionLetterFilestoreId;
     let tenant = data?.tenantId || tenantId;
+    const prevGetLang = Digit.StoreData.getCurrentLanguage;
+    try {
+      if (!fileStoreId) {
+        const nowIST = new Date().toLocaleString("en-GB", { timeZone: "Asia/Kolkata", hour12: false }).replace(",", "") + " IST";
+        Digit.StoreData.getCurrentLanguage = () => "pn_IN";
+        const newValidityDate = new Date(data?.applicationData?.approvalDate);
 
-    if(!fileStoreId) {
-    const nowIST = new Date().toLocaleString('en-GB', { timeZone: 'Asia/Kolkata', hour12: false }).replace(',', '') + ' IST';
+        // validity date = approval date + 3 as per feedback
+        newValidityDate.setFullYear(newValidityDate.getFullYear() + 3);
+        const approvalDatePlusThree = newValidityDate.getTime();
 
-    const newValidityDate = new Date(data?.applicationData?.approvalDate);
+        const designation = ulbType === "Municipal Corporation" ? "Municipal Commissioner" : "Executive Officer";
+        const requestData = {
+          ...data?.applicationData,
+          edcrDetail: [{ ...data?.edcrDetails }],
+          subjectLine,
+          fileno,
+          nowIST,
+          newValidityDate,
+          designation,
+          approverComment: comments,
+        };
+        let count = 0;
+        for (let i = 0; i < workflowDetails?.data?.processInstances?.length; i++) {
+          if (
+            (workflowDetails?.data?.processInstances[i]?.action === "POST_PAYMENT_APPLY" ||
+              workflowDetails?.data?.processInstances[i]?.action === "PAY") &&
+            workflowDetails?.data?.processInstances?.[i]?.state?.applicationStatus === "APPROVAL_INPROGRESS" &&
+            count == 0
+          ) {
+            requestData.additionalDetails.submissionDate = workflowDetails?.data?.processInstances[i]?.auditDetails?.createdTime;
+            count = 1;
+          }
+        }
+        if (stakeholderAddress && requestData && requestData?.additionalDetails) {
+          requestData.additionalDetails.stakeholderAddress = stakeholderAddress;
+        }
+        // if (requestData && requestData?.additionalDetails?.signature?.signURL) {
+        //   const result = await getBase64Img(requestData?.additionalDetails?.signature?.signURL, state);
+        //   requestData.additionalDetails.signature = { ...requestData.additionalDetails.signature, base64Signature: result };
+        // }
 
-    // validity date = approval date + 3 as per feedback
-    newValidityDate.setFullYear(newValidityDate.getFullYear() + 3);
-    const approvalDatePlusThree = newValidityDate.getTime();
-
-
-    const designation = ulbType === "Municipal Corporation" ? "Municipal Commissioner" : "Executive Officer";
-    const requestData = { ...data?.applicationData, edcrDetail: [{ ...data?.edcrDetails }], subjectLine , fileno, nowIST, newValidityDate,designation , approverComment: comments}
-    let count = 0
-    for (let i = 0; i < workflowDetails?.data?.processInstances?.length; i++) {
-      if (
-        (workflowDetails?.data?.processInstances[i]?.action === "POST_PAYMENT_APPLY" ||
-          workflowDetails?.data?.processInstances[i]?.action === "PAY") &&
-        workflowDetails?.data?.processInstances?.[i]?.state?.applicationStatus === "APPROVAL_INPROGRESS" &&
-        count == 0
-      ) {
-        requestData.additionalDetails.submissionDate =
-          workflowDetails?.data?.processInstances[i]?.auditDetails?.createdTime
-        count = 1
+        if (requestData?.additionalDetails?.approvedColony == "NO") {
+          requestData.additionalDetails.permitData =
+            "The plot has been officially regularized under No. " +
+            requestData?.additionalDetails?.NocNumber +
+            "  dated " +
+            requestData?.additionalDetails?.nocObject?.approvedOn +
+            " , registered in the name of " +
+            requestData?.additionalDetails?.nocObject?.applicantOwnerOrFirmName +
+            ". This regularization falls within the jurisdiction of " +
+            requestData?.additionalDetails?.UlbName +
+            ".Any form of misrepresentation of the NoC is strictly prohibited. Such misrepresentation renders the building plan null and void, and it will be regarded as an act of impersonation. Criminal proceedings will be initiated against the owner and concerned architect / engineer/ building designer / supervisor involved in such actions";
+        } else if (requestData?.additionalDetails?.approvedColony == "YES") {
+          requestData.additionalDetails.permitData =
+            "The building plan falls under approved colony " + requestData?.additionalDetails?.nameofApprovedcolony;
+        } else if (requestData?.additionalDetails?.approvedColony == "Colony Prior to 1995 (colony name)") {
+          requestData.additionalDetails.permitData =
+            "The building plan falls under Colonies prior to 1995  " + requestData?.additionalDetails?.nameofApprovedcolony;
+        } else if (requestData?.additionalDetails?.approvedColony == "Stand Alone Projects") {
+          requestData.additionalDetails.permitData = "The building plan falls under Stand-Alone Project.";
+        } else {
+          requestData.additionalDetails.permitData = "The building plan falls under Lal Lakir";
+        }
+        const response = await Digit.PaymentService.generatePdf(tenantId, { Bpa: [requestData] }, order);
+        fileStoreId = response?.filestoreIds[0];
+        tenant = tenantId;
       }
+    } catch (error) {
+      console.log("err", error);
+    } finally {
+      Digit.StoreData.getCurrentLanguage = prevGetLang;
     }
     if (stakeholderAddress && requestData && requestData?.additionalDetails) {
       requestData.additionalDetails.stakeholderAddress = stakeholderAddress;
@@ -833,7 +880,7 @@ useEffect(() => {
     const response = await Digit.PaymentService.generatePdf(tenantId, { Bpa: [requestData] }, order)
     fileStoreId = response?.filestoreIds[0]
     tenant = tenantId
-  }
+  
     const fileStore = await Digit.PaymentService.printReciept(tenant, { fileStoreIds: fileStoreId })
     window.open(fileStore[fileStoreId], "_blank")
     requestData["applicationType"] = data?.applicationData?.additionalDetails?.applicationType
@@ -901,6 +948,7 @@ useEffect(() => {
   }
 
    async function getPermitOccupancyOrderSearchFilestore({ tenantId }, order, mode = "download") {
+     const prevGetLang = Digit.StoreData.getCurrentLanguage;
      try {
        setIsEnableLoader(true);
        const nowIST = new Date().toLocaleString("en-GB", { timeZone: "Asia/Kolkata", hour12: false }).replace(",", "") + " IST";
@@ -975,6 +1023,7 @@ useEffect(() => {
      } catch (error) {
        console.log("error", error);
      } finally {
+       Digit.StoreData.getCurrentLanguage = prevGetLang;
        setIsEnableLoader(true);
      }
    }

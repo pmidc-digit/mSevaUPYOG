2aec383f3587319509ba2eaf6c5452176ea17491
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-05-29 11:32:52 +0530

    fire noc application form pdf service config integration

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/Firenoc/src/pageComponents/FireNOCApplicationOverview.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/Firenoc/src/pageComponents/FireNOCApplicationOverview.js
@@ -254,31 +254,11 @@ const FireNOCApplicationOverview = () => {
   
     const isResumable = appStatus === "INITIATED";
 
-  const handleDownloadPdf = async () => {
-    try {
-      setLoading(true);
-      const Property = fireNOC;
-      const tenantInfo = tenants.find((tenant) => tenant.code === Property.tenantId);
-      console.log('tenantInfo', tenantInfo)
-      // const site = Property?.nocDetails?.additionalDetails?.siteDetails;
-      const ulbType = tenantInfo?.city?.ulbType;
-      const ulbName = tenantInfo?.city?.name;
-
-      const acknowledgementData = await getNOCAcknowledgementData(Property, tenantInfo, ulbType, ulbName, t);
-      setTimeout(() => {
-        Digit.Utils.pdf.generateFormattedFireNoc(acknowledgementData);
-      }, 0);
-    } catch (error) {
-      // console.error("Error generating acknowledgement:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
 
   const getRecieptSearch = async ({ tenantId, payments, pdfkey, EmpData = null, ...params }) => {
     try {
       setLoading(true);
-      const nocSanctionData = await getNOCSanctionLetter(details, t, EmpData,);
+      const nocSanctionData = await getNOCSanctionLetter(fireNOC, t, EmpData,);
       let filestoreID = null;
         try {
           const response = await Digit.PaymentService.generatePdf(
@@ -345,13 +325,13 @@ const FireNOCApplicationOverview = () => {
         },
         {
           label: t("NOC_APPLICATION_FORM"),
-          onClick: handleDownloadPdf,
+          onClick: () => getRecieptSearch({ tenantId: paymentDetail?.tenantId, payments: payment, pdfkey: "firenoc-application" }),
         },
       ]
       : [
         {
           label: t("NOC_APPLICATION_FORM"),
-          onClick: handleDownloadPdf,
+          onClick: () => getRecieptSearch({ tenantId: paymentDetail?.tenantId, payments: payment, pdfkey: "firenoc-application" }),
         },
       ];
 

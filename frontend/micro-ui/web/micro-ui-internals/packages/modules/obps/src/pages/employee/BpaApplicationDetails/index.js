773ac8d09ccb6d3d58330d3a0a402f6c9a12fd37
NayanPMIDC <nayandhawan@kpmg.com>
2026-06-01 08:01:41 +0530

    feat: eSign Drawing Document

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/employee/BpaApplicationDetails/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/employee/BpaApplicationDetails/index.js
@@ -86,7 +86,7 @@ const CloseBtn = (props) => {
 };
 
 const BpaApplicationDetail = () => {
-  const { bpaid, tenant } = useParams();
+  const { bpaid, tenant, filestore } = useParams();
   const id = decryptId(bpaid)
   const { t } = useTranslation();
   // const tenantId = Digit.ULBService.getCurrentTenantId();
@@ -125,7 +125,7 @@ const BpaApplicationDetail = () => {
   const [showModal, setShowModal] = useState(false);
   const [showImageModal, setShowImageModal] = useState(false);
   const [imageUrl, setImageUrl] = useState(null);
-  let { id: applicationNumber } = useParams();
+  // let { id: applicationNumber } = useParams();
   const [isEnableLoader, setIsEnableLoader] = useState(false);
   const [checklistRemarks, setChecklistRemarks] = useState({});
   const [getLoader, setLoader] = useState(false);
@@ -1740,7 +1740,8 @@ const BpaApplicationDetail = () => {
 
       const fileStoreId = await getPermitOccupancyOrderSearchFilestore({tenantId}, "buildingpermit-normal");
 
-      const callbackUrl = `${window.location.origin}/digit-ui/employee/obps/bpa/esign/complete/${id}`;
+      const callbackUrl = `${window.location.origin}/digit-ui/employee/obps/filestore/${id}`;
+      // const callbackUrl = `${window.location.origin}/digit-ui/employee/obps/bpa/esign/complete/${id}`;
       const authToken = localStorage.getItem('token');
 
       // Trigger eSign
@@ -2596,7 +2597,7 @@ const BpaApplicationDetail = () => {
             action={selectedAction}
             tenantId={tenantId}
             state={stateId}
-            id={applicationNumber}
+            id={id}
             applicationDetails={data}
             applicationData={data?.applicationData}
             closeModal={closeModal}

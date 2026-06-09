0eb1e31e82641989854bde91112a8f86c0b9d52d
narinder <tarun@Taruns-MacBook-Air.local>
2026-06-09 14:24:41 +0530

    fix: library ndc

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/index.js
@@ -181,6 +181,7 @@ import useTLApplicationDetail from "./tl/useApplicationDetail";
 import useTLApplicationActions from "./tl/useApplicationActions";
 import useTLFetchBill from "./tl/useFetchBill";
 import useNDCDocumentSearch from "./ndc/useNDCDocumentSearch";
+import useSearchNDCWS from "./ndc/useSearchWS";
 
 import useTLGenderMDMS from "./tl/useTLGenderMDMS";
 import useTLInbox from "./tl/useInbox";
@@ -255,7 +256,12 @@ import useSurveyQuestionInbox from "./surveys/useSurveyQuestionInbox";
 
 import useNOCDetails from "./noc/useNOCDetails";
 import useNOCInbox from "./noc/useInbox";
-import { useNOCSearchApplication, useNOCCitizenSearchApplication, useNOCSearchApplicationByIdOrMobile, useNOCCheckListSearch } from "./noc/useSearchApplications";
+import {
+  useNOCSearchApplication,
+  useNOCCitizenSearchApplication,
+  useNOCSearchApplicationByIdOrMobile,
+  useNOCCheckListSearch,
+} from "./noc/useSearchApplications";
 import useBuildingCategory from "./noc/useBuildingCategory";
 import useBuildingType from "./noc/useBuildingType";
 import useRoadType from "./noc/useRoadType";
@@ -418,7 +424,12 @@ import useGCInbox from "./gc/useInbox";
 
 //CLU
 import useCLUFeeCalculator from "./obps/useCLUFeeCalculator";
-import { useCLUSearchApplication, useCLUCitizenSearchApplication, useCLUSearchApplicationByIdOrMobile, useCLUCheckListSearch } from "./obps/useCLUSearchApplication";
+import {
+  useCLUSearchApplication,
+  useCLUCitizenSearchApplication,
+  useCLUSearchApplicationByIdOrMobile,
+  useCLUCheckListSearch,
+} from "./obps/useCLUSearchApplication";
 import useCLUInbox from "./obps/useCLUInbox";
 import useLudhianaPropertSearch from "./pt/useLudhianaPropertSearch";
 import useBathindaPropertySearch from "./pt/useBathindaPropertySearch";
@@ -662,7 +673,7 @@ const obps = {
   useLayoutSearchApplicationByIdOrMobile,
   useCLUInbox,
   useLayoutBuildingCategory,
-  useBPACheckListSearch
+  useBPACheckListSearch,
 };
 
 const events = {
@@ -776,7 +787,7 @@ const noc = {
   useNOCFeeCalculator,
   useNOCSearchApplicationByIdOrMobile,
   useNOCCheckListSearch,
-  useNocCreateAPI
+  useNocCreateAPI,
 };
 
 const ndc = {
@@ -784,6 +795,7 @@ const ndc = {
   useSearchApplication: useNDCSearchApplication,
   useSearchEmployeeApplication: useNDCSearchApplicationEmployee,
   useNDCDocumentSearch,
+  useSearchWS: useSearchNDCWS,
 };
 
 const challangeneration = {

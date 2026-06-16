f69a6ccee9e4000a29f1b123b9f8c5650ad845a8
vidhi Rajvanshi <101122567+programvidhi89@users.noreply.github.com>
2026-06-16 15:03:02 +0530

    upgrade service from jdk 8 to 21

--- core-services/egov-url-shortening/src/main/java/org/egov/url/shortening/producer/Producer.java
+++ core-services/egov-url-shortening/src/main/java/org/egov/url/shortening/producer/Producer.java
@@ -2,7 +2,7 @@ package org.egov.url.shortening.producer;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.egov.tracer.kafka.CustomKafkaTemplate;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 public class Producer {
 
     @Autowired
-    private CustomKafkaTemplate<String, Object> kafkaTemplate;
+    private KafkaTemplate<String, Object> kafkaTemplate;
 
     public void push(String topic, Object value) {
         kafkaTemplate.send(topic, value);

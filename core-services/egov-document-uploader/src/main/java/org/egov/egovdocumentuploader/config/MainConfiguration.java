54e5ddbfd00d76df96c1f5db7fea25555f07575d
vidhi Rajvanshi <101122567+programvidhi89@users.noreply.github.com>
2026-06-26 14:23:45 +0530

    upgrade service from jdk 8 to 21

--- core-services/egov-document-uploader/src/main/java/org/egov/egovdocumentuploader/config/MainConfiguration.java
+++ core-services/egov-document-uploader/src/main/java/org/egov/egovdocumentuploader/config/MainConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import java.util.TimeZone;
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.egov.tracer.config.TracerConfiguration;
@@ -30,10 +30,26 @@ public class MainConfiguration {
     }
 
     @Bean
-    @Autowired
     public MappingJackson2HttpMessageConverter jacksonConverter(ObjectMapper objectMapper) {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(objectMapper);
         return converter;
+    }
+
+    @Value("${kafka.config.bootstrap_server_config}")
+    private String bootstrapServers;
+
+    @Bean
+    public org.springframework.kafka.core.ProducerFactory<String, Object> producerFactory() {
+        java.util.Map<String, Object> configProps = new java.util.HashMap<>();
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+        return new org.springframework.kafka.core.DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public org.springframework.kafka.core.KafkaTemplate<String, Object> kafkaTemplate() {
+        return new org.springframework.kafka.core.KafkaTemplate<>(producerFactory());
     }
 }

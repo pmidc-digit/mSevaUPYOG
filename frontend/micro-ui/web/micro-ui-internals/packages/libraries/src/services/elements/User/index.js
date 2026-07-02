5544d07448368cb553daea30b4b2fd9f13efdbb5
anujkit <chauhan.chauhan5@gmail.com>
2026-07-02 15:14:50 +0530

    PI-21899 The API_ONLY role should not allow UI login.

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/User/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/elements/User/index.js
@@ -87,7 +87,7 @@ export const UserService = {
       if (userType === "citizen") {
         window.location.replace("/digit-ui/citizen");
       } else {
-        window.location.replace("/digit-ui/employee/user/language-selection");
+        window.location.replace("/digit-ui/employee");
       }
     }
   },

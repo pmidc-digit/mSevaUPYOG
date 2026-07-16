1d7186a348d0ed58f66aa92f4c650974fd8d4358
Anshika Jain <anshikaj778@gmail.com>
2026-07-16 16:31:01 +0530

    clu employee doc issue

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pageComponents/CLUDocumentCheckList.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pageComponents/CLUDocumentCheckList.js
@@ -15,7 +15,14 @@ const CLUDocumentChecklist = ({ documents, applicationNo, tenantId, onRemarksCha
 
   // fetch urls and checklist data as before...
   const { data: urlsList, isLoading } = Digit.Hooks.noc.useNOCDocumentSearch(
-    { value: { workflowDocs: (documents || []).map((d) => ({ documentUid: d.documentUid })) } },
+    {
+      value: {
+        workflowDocs: (documents || []).map((d) => ({
+          documentUid: d.documentUid,
+          documentAttachment: d.documentAttachment || "",
+        })),
+      },
+    },
     { enabled: documents?.length > 0 }
   );
   const { data: searchChecklistData } = Digit.Hooks.obps.useCLUCheckListSearch({ applicationNo }, tenantId);

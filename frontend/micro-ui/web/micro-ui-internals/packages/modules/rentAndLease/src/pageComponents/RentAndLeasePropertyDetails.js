846d7f6bf930a94f42d84946d997aa7bb85de752
narinder <tarun@Taruns-MacBook-Air.local>
2026-07-22 16:01:25 +0530

    fix: rl observations

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/rentAndLease/src/pageComponents/RentAndLeasePropertyDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/rentAndLease/src/pageComponents/RentAndLeasePropertyDetails.js
@@ -39,20 +39,25 @@ const RentAndLeasePropertyDetails = ({ onGoBack, goNext, currentStepData, valida
     { name: t("New"), code: "new" },
   ];
 
-  const incrementPeriodMonthsValues = [
-    { name: 1, code: "1" },
-    { name: 2, code: "2" },
-    { name: 3, code: "3" },
-    { name: 4, code: "4" },
-    { name: 5, code: "5" },
-    { name: 6, code: "6" },
-    { name: 7, code: "7" },
-    { name: 8, code: "8" },
-    { name: 9, code: "9" },
-    { name: 10, code: "10" },
-    { name: 11, code: "11" },
-    { name: 12, code: "12" },
-  ];
+  // const incrementPeriodMonthsValues = [
+  //   { name: 1, code: "1" },
+  //   { name: 2, code: "2" },
+  //   { name: 3, code: "3" },
+  //   { name: 4, code: "4" },
+  //   { name: 5, code: "5" },
+  //   { name: 6, code: "6" },
+  //   { name: 7, code: "7" },
+  //   { name: 8, code: "8" },
+  //   { name: 9, code: "9" },
+  //   { name: 10, code: "10" },
+  //   { name: 11, code: "11" },
+  //   { name: 12, code: "12" },
+  // ];
+
+  const incrementPeriodMonthsValues = Array.from({ length: 60 }, (_, index) => ({
+    name: index + 1,
+    code: String(index + 1),
+  }));
 
   const propertySpecificOptions = [
     { name: t("COMMERCIAL"), code: "Commercial", i18nKey: "Commercial" },
@@ -127,6 +132,10 @@ const RentAndLeasePropertyDetails = ({ onGoBack, goNext, currentStepData, valida
       // tax_applicable: null,
     },
   });
+
+  useEffect(() => {
+    console.log("errors", errors);
+  }, [errors]);
 
   const docUploadData = {
     Challan: {
@@ -726,10 +735,10 @@ const RentAndLeasePropertyDetails = ({ onGoBack, goNext, currentStepData, valida
           </LabelFieldPair>
           {errors.arrear && <CardLabelError className="ral-error-label">{getErrorMessage("arrear")}</CardLabelError>}
 
-          {/* Last Billing Period */}
+          {/* Last Billing Month */}
           <LabelFieldPair>
             <CardLabel>
-              {t("Last Billing Period")} {watch("arrear") > 0 && <span className="mandatory-asterisk">*</span>}
+              {t("Last Billing Month")} {watch("arrear") > 0 && <span className="mandatory-asterisk">*</span>}
             </CardLabel>
             <div className="form-field">
               <Controller

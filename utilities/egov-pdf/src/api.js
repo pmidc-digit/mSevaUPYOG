419d63878c96f96b43ec0a4b093661b5d8f20473
anujkit <chauhan.chauhan5@gmail.com>
2026-08-05 16:13:26 +0530

    additional details check

--- utilities/egov-pdf/src/api.js
+++ utilities/egov-pdf/src/api.js
@@ -834,6 +834,9 @@ async function create_bulk_pdf(kafkaData) {
         if (waterBillsData.length > 0) {
           for (let waterBill of waterBillsData) {
             if (waterBill.status === 'ACTIVE' && waterBill.totalAmount > 0) {
+              if (!waterBill.additionalDetails) {
+                waterBill.additionalDetails = {};
+              }
               waterBill.additionalDetails.latePaymentc = (waterBillsData[0].tenantId == "pb.patiala" || waterBillsData[0].tenantId == "pb.nabha") ? '0% Late payment charges will be applied after due date' : '10% late payment charges For both water and sewerage';
               consolidatedResult.Bill.push(waterBill);
             }
@@ -862,6 +865,9 @@ async function create_bulk_pdf(kafkaData) {
         if (sewerageBillsData.length > 0) {
           for (let sewerageBill of sewerageBillsData) {
             if (sewerageBill.status === 'ACTIVE' && sewerageBill.totalAmount > 0) {
+              if (!sewerageBill.additionalDetails) {
+                sewerageBill.additionalDetails = {};
+              }
               sewerageBill.additionalDetails.latePaymentc = (sewerageBillsData[0].tenantId == "pb.patiala" || sewerageBillsData[0].tenantId == "pb.nabha") ? '25% Late payment charges will be applied after due date' : '10% late payment charges For both water and sewerage';
               consolidatedResult.Bill.push(sewerageBill);
             }

5dddff9ad57c41d8023615435e35c23d17128c8d
anujkit <46367271+anujkit@users.noreply.github.com>
2026-08-06 00:13:29 +0530

    Change PDF_BATCH_SIZE default from 40 to 20

--- utilities/egov-pdf/src/config.js
+++ utilities/egov-pdf/src/config.js
@@ -13,7 +13,7 @@ module.exports = {
   KAFKA_BROKER_HOST: process.env.KAFKA_BROKER_HOST || "localhost:9092",
   KAFKA_RECEIVE_CREATE_JOB_TOPIC: process.env.KAFKA_RECEIVE_CREATE_JOB_TOPIC || "PDF_GEN_RECEIVE",
   KAFKA_BULK_PDF_TOPIC: process.env.KAFKA_BULK_PDF_TOPIC || "BULK_PDF_GEN",
-  PDF_BATCH_SIZE: process.env.PDF_BATCH_SIZE || 40,
+  PDF_BATCH_SIZE: process.env.PDF_BATCH_SIZE || 20,
   DB_USER: process.env.DB_USER || "egovprod",
   DB_PASSWORD: process.env.DB_PASSWORD || "jr88WZdM#4YU##1Q",
   DB_HOST: process.env.DB_HOST || "10.44.237.25",

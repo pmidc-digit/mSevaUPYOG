1b0ce3149b6d4d2c1b33be4c126be7122a347473
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-08-10 11:56:42 +0530

    layout forward action fix

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/Applications/LayoutApplicationSummary.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/Applications/LayoutApplicationSummary.js
@@ -542,11 +542,11 @@ const LayoutApplicationOverview = () => {
       });
 
   const modifiedActions = actions?.map((action) => ({
-      ...action,
-      action: action.action?.toUpperCase().includes("FORWARD")
-        ? "FORWARD"
-        : action.action,
-      }));
+  ...action,
+  forcedName: action.action?.toUpperCase().includes("FORWARD")
+    ? "CS_ACTION_FORWARD"
+    : undefined,
+}));
   function onActionSelect(action) {
     const appNo = applicationDetails?.Layout?.[0]?.applicationNo
 

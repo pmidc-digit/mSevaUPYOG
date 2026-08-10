9b3f0dfe62b59f71a2ccf0d3544210e5fdf9a1ce
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-08-10 10:37:14 +0530

    layout loi and esign integration

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/Module.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/Module.js
@@ -85,6 +85,7 @@ import CLUInbox from "./pages/employee/cluInbox/CLUInbox"
 import CLUEmployeeApplicationDetails from "./pages/employee/ApplicationOverview/CLUApplicationOverview";
 import CLUEsignResponse from "./pages/employee/CLUEsignResponse";
 import LayoutEmployeeApplicationDetails from "./pages/employee/ApplicationOverview/LayoutApplicationOverview";
+import LayoutEsignResponse from "./pages/employee/LayoutEsignResponse";
 
 import LayoutStepperForm from "./pages/citizen/LayoutStepper/LayoutStepperForm";
 import LayoutStepFormOne from "./pages/citizen/LayoutStepper/LayoutStepFormOne";
@@ -295,6 +296,7 @@ const componentsToRegister = {
   CLUInbox,
   CLUEmployeeApplicationDetails,
   CLUEsignResponse,
+  LayoutEsignResponse,
   OCStepFormOne,
   OCStepFormTwo,
   OCStepFormThree,

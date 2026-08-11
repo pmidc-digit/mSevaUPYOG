69a5bb03f3c76d37871c37a435a5efffb1fc3694
Anshika Jain <anshikaj778@gmail.com>
2026-08-11 18:36:31 +0530

    fix: disabled qualtype for professional reg correction

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ApplicationDetail/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ApplicationDetail/index.js
@@ -274,6 +274,7 @@ const licenseType = t(`TRADELICENSE_TRADETYPE_${License?.tradeLicenseDetail?.tra
       result: {
         Licenses: [{ ...data }],
       },
+      isPaid: reciept_data?.Payments?.length > 0 || false,
       initiationFlow: true,
       editableFields: {
         "provide-license-type": true,

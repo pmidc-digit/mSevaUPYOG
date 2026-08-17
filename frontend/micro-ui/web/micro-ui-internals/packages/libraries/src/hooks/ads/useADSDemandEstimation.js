77caa8ecace89eec1b9a052ee5b990444ee1d219
narinder <tarun@Taruns-MacBook-Air.local>
2026-08-17 13:02:39 +0530

    fix: code merge ads and rent and lease

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/ads/useADSDemandEstimation.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/ads/useADSDemandEstimation.js
@@ -1,19 +1,14 @@
 import { useQuery, useMutation } from "react-query";
 
-import { ADSServices } from "../../services/elements/ADS"
+import { ADSServices } from "../../services/elements/ADS";
 
 /*Custom hook  for managing demand estimation actions
  * (either creation or updating) through ADS services*/
 
-const useADSDemandEstimation =  (tenantId, type = true) => {
-  
+const useADSDemandEstimation = (tenantId, type = true) => {
   if (type) {
-   
-    return useMutation((data) => 
-      ADSServices.estimateCreate(data, tenantId));
-    
-  } 
-  else {
+    return useMutation((data) => ADSServices.estimateCreate(data, tenantId));
+  } else {
     return useMutation((data) => ADSServices.update(data, tenantId));
   }
 };

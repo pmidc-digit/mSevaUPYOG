8bbd6a1709beb6ace4f9c7020ee46815c0befc0a
narinder <tarun@Taruns-MacBook-Air.local>
2026-08-18 18:01:03 +0530

    remove css

--- frontend/micro-ui/web/src/index.js
+++ frontend/micro-ui/web/src/index.js
@@ -3,7 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./theme.css";
-import "../micro-ui-internals/packages/css/dist/index.css";
 import App from "./App";
 import { TLCustomisations } from "./Customisations/tl/TLCustomisation";
 

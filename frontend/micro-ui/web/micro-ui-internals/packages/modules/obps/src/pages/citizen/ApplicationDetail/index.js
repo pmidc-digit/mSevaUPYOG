1866834f3d0bbb17ff4d6f90cd5ba2de8f568b54
Anshika Jain <anshikaj778@gmail.com>
2026-08-19 15:20:09 +0530

    fix: professional reg button localisation

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ApplicationDetail/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/ApplicationDetail/index.js
@@ -752,7 +752,13 @@ const dob = typeof License?.tradeLicenseDetail?.owners?.[0]?.dob === "string" ? 
             {displayMenu ? (
               <Menu
                 localeKeyPrefix="BPA_PROFESSIONAL"
-                options={actions}
+                options={actions?.map((action) => ({
+                  ...action,
+                  forcedName:
+                    action?.action === "EDIT"
+                      ? `BPA_PROFESSIONAL_${action?.action}`
+                      : action?.action,
+                }))}
                 optionKey={"action"}
                 t={t}
                 onSelect={onActionSelect}

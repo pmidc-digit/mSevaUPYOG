297c0cf8a8a5f549d1a3aa1fe839147078478e1d
Anshika Jain <anshikaj778@gmail.com>
2026-08-24 10:31:01 +0530

    fix: bpa daft comment carry forward issue

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/employee/BpaApplicationDetails/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/employee/BpaApplicationDetails/index.js
@@ -1738,6 +1738,7 @@ const BpaApplicationDetail = () => {
             // geoLocations: data?.BPA?.action === "SEND_FOR_INSPECTION_REPORT" && (userInfo?.info?.roles.filter(role => role.code === "BPA_FIELD_INSPECTOR")).length > 0 ? geoLocations : data?.BPA?.additionalDetails?.geoLocations,
             // FieldReports: appData?.applicationData?.status === "INSPECTION_REPORT_PENDING" && (userInfo?.info?.roles.filter(role => role.code === "BPA_FIELD_REPORT_INSPECTOR")).length > 0 ? canSubmit : null,
             fieldinspection_pending: fieldInspectionPending,
+            draftComment: ""
           },
         },
       };
@@ -1763,6 +1764,7 @@ const BpaApplicationDetail = () => {
             setTimeout(closeToast, 5000);
           },
           onSuccess: (data, variables) => {
+            setDraftComment("");
             sessionStorage.removeItem("WS_SESSION_APPLICATION_DETAILS");
             setIsEnableLoader(false);
             if (isOBPS?.bpa) {

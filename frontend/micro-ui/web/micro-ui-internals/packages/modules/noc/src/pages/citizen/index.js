d7c49c44e28b01f5084256982a674dab7421b1d0
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-09-02 10:06:27 +0530

    bpa breadcrumb addition

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/citizen/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/noc/src/pages/citizen/index.js
@@ -22,7 +22,8 @@ const NOCBreadCrumbs = ({ location, cameFromOBPS }) => {
       location.pathname.includes("/noc/new-application") ||
       location.pathname.includes("noc/my-application") ||
       location.pathname.includes("noc/search/application-overview/") ||
-      location.pathname.includes("noc/search-application");
+      location.pathname.includes("noc/search-application")||
+      location.pathname.includes("noc/noc-my-application");
 
     breadcrumbs.push(
       <span key="home">

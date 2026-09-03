6a7b12c8f10ca9a70dae45b3bc0ad01497ef356d
roshan2611 <roshanchoudhary60@gmail.com>
2026-09-03 19:12:29 +0530

    Update Storage Service

--- core-services/egov-filestore/src/main/java/org/egov/filestore/domain/service/StorageService.java
+++ core-services/egov-filestore/src/main/java/org/egov/filestore/domain/service/StorageService.java
@@ -42,7 +42,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StorageService {
 
-	private final CompressionService compressionService;
+	@Autowired
+	private CompressionService compressionService;
 
 	@Autowired
 	private CloudFileMgrUtils util;
@@ -82,14 +83,14 @@ public class StorageService {
 
 	@Autowired
 	public StorageService(ArtifactRepository artifactRepository, IdGeneratorService idGeneratorService,
-			FileStoreConfig fileStoreConfig, StorageValidator storageValidator, FileStoreConfig configs, MinioConfig minioConfig, CompressionService compressionService) {
+			FileStoreConfig fileStoreConfig, StorageValidator storageValidator, FileStoreConfig configs, MinioConfig minioConfig) {
 		this.artifactRepository = artifactRepository;
 		this.idGeneratorService = idGeneratorService;
 		this.fileStoreConfig = fileStoreConfig;
 		this.storageValidator = storageValidator;
 		this.minioConfig = minioConfig;
 		this.configs = configs;
-		this.compressionService = compressionService;
+		
 	}
 
 	public List<String> save(List<MultipartFile> filesToStore, String module, String tag, String tenantId, RequestInfo requestInfo) {

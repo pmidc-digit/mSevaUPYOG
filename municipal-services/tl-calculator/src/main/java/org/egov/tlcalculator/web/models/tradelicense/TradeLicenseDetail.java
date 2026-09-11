40c92564c606663a1b6a3c886ff885516df7e807
manjotkaurPMIDC <manjotkaur4@kpmg.com>
2026-09-11 14:27:09 +0530

    LTS

--- municipal-services/tl-calculator/src/main/java/org/egov/tlcalculator/web/models/tradelicense/TradeLicenseDetail.java
+++ municipal-services/tl-calculator/src/main/java/org/egov/tlcalculator/web/models/tradelicense/TradeLicenseDetail.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.JsonNode;
 
 import io.swagger.annotations.ApiModel;
 
@@ -127,9 +127,9 @@ public class TradeLicenseDetail   {
     @JsonProperty("verificationDocuments")
     @Valid
     private List<Document> verificationDocuments = null;
-    
+
     @JsonProperty("additionalDetail")
-    private ObjectNode additionalDetail = null;
+    private JsonNode additionalDetail = null;
 
     @JsonProperty("institution")
     private Institution institution = null;

a06df9ee05b1e318272ddd1fe9c75230c7a39f1d
anujkit <46367271+anujkit@users.noreply.github.com>
2026-09-15 22:23:29 +0530

    Remove sensitive log statement in loginUser
    Removed logging of hardcoded password in loginUser method.

--- core-services/xstate-chatbot/nodejs/src/session/user-service.js
+++ core-services/xstate-chatbot/nodejs/src/session/user-service.js
@@ -80,7 +80,6 @@ class UserService {
 
   async loginUser(mobileNumber, tenantId) {
     console.log("Into Login User mobileNumber", mobileNumber, "tenant id", tenantId);
-    console.log("Into Login User mobileNumber", config.userService.userServiceHardCodedPassword);
     let data = new URLSearchParams();
     data.append('grant_type', 'password');
     data.append('scope', 'read');

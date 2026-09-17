4dfc13befaf0f41af94454e456303336cd78f447
Anoushka Srivastava <92670920+anou1234@users.noreply.github.com>
2026-09-17 12:58:22 +0530

    bpa rejection letter integration

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pages/citizen/BpaApplicationDetail/index.js
@@ -846,6 +846,62 @@ const BpaApplicationDetail = () => {
     requestData["applicationType"] = data?.applicationData?.additionalDetails?.applicationType;
   }
 
+  async function getRejectionLetter({ tenantId }, order, mode = "download") {
+    let tenant = data?.tenantId || tenantId;
+    const prevGetLang = Digit.StoreData.getCurrentLanguage;
+    let fileStoreId;
+    try {
+        const nowIST = new Date().toLocaleString("en-GB", { timeZone: "Asia/Kolkata", hour12: false }).replace(",", "") + " IST";
+        Digit.StoreData.getCurrentLanguage = () => "pn_IN";
+
+        const designation = ulbType === "Municipal Corporation" ? "Municipal Commissioner" : "Executive Officer";
+        const requestData = {
+          ...data?.applicationData,
+          edcrDetail: [{ ...data?.edcrDetails }],
+          subjectLine,
+          fileno,
+          nowIST,
+          designation,
+          approverComment: comments,
+        };
+        if (requestData?.landInfo?.owners) {
+          requestData.landInfo = {
+            ...requestData.landInfo,
+            owners: requestData.landInfo.owners.map((owner) => ({
+              ...owner,
+              permanentPinCode: owner?.permanentPinCode || addressPincode || " ",
+            })),
+          };
+        }
+        let count = 0;
+        for (let i = 0; i < workflowDetails?.data?.processInstances?.length; i++) {
+          if (
+            (workflowDetails?.data?.processInstances[i]?.action === "POST_PAYMENT_APPLY" ||
+              workflowDetails?.data?.processInstances[i]?.action === "PAY") &&
+            workflowDetails?.data?.processInstances?.[i]?.state?.applicationStatus === "APPROVAL_INPROGRESS" &&
+            count == 0
+          ) {
+            requestData.additionalDetails.submissionDate = workflowDetails?.data?.processInstances[i]?.auditDetails?.createdTime;
+            count = 1;
+          }
+        }
+        if (stakeholderAddress && requestData && requestData?.additionalDetails) {
+          requestData.additionalDetails.stakeholderAddress = stakeholderAddress;
+        }
+        const response = await Digit.PaymentService.generatePdf(tenantId, { Bpa: [requestData] }, order);
+        fileStoreId = response?.filestoreIds[0];
+        tenant = tenantId;
+      }
+     catch (error) {
+      console.log("err", error);
+    } finally {
+      Digit.StoreData.getCurrentLanguage = prevGetLang;
+    }
+    const fileStore = await Digit.PaymentService.printReciept(tenant, { fileStoreIds: fileStoreId });
+    window.open(fileStore[fileStoreId], "_blank");
+    requestData["applicationType"] = data?.applicationData?.additionalDetails?.applicationType;
+  }
+
   async function getPermitOccupancyOrderSearchReturnFilestore({ tenantId }, order, mode = "download") {
     const prevGetLang = Digit.StoreData.getCurrentLanguage;
     try {
@@ -1926,6 +1982,13 @@ const BpaApplicationDetail = () => {
       order: 4,
       label: t("BPA_COMPARISON_REPORT_LABEL"),
       onClick: () => window.open(data?.comparisionReport?.comparisonReport, "_blank"),
+    });
+  }
+  if (data?.applicationData?.status === "REJECTED") {
+    dowloadOptions.push({
+      order: 5,
+      label: t("BPA_REJECTION_LETTER"),
+      onClick: () => getRejectionLetter({ tenantId: data?.applicationData?.tenantId }, "bpa-rejection-letter"),
     });
   }
 
